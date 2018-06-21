0dcfbf155dad8f108e1b7fa26996fe2b193d2055
Stefan Hajnoczi <stefanha@redhat.com>
2018-06-21 09:34:25 +0100

    UPSTREAM: fuse: export fuse_len_args()
    virtio-fs will need to query the length of fuse_arg lists.  Make the symbol
    visible.
    
    Change-Id: Ib5f974c4ca79d1316445a8d221c4974480471f31
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1022,4 +1022,9 @@ ssize_t fuse_passthrough_read_iter(struct kiocb *iocb, struct iov_iter *to);
 ssize_t fuse_passthrough_write_iter(struct kiocb *iocb, struct iov_iter *from);
 
 
+/**
+ * Return the number of bytes in an arguments list
+ */
+unsigned int fuse_len_args(unsigned int numargs, struct fuse_arg *args);
+
 #endif /* _FS_FUSE_I_H */
