3bfc0c4702b7fc926eb6ac8e39357ba0d208713b
Stefan Hajnoczi <stefanha@redhat.com>
2018-06-22 13:48:30 +0100

    UPSTREAM: fuse: export fuse_get_unique()
    virtio-fs will need unique IDs for FORGET requests from outside
    fs/fuse/dev.c.  Make the symbol visible.
    
    Change-Id: Ifc05f61308136d6612191b34f8c3834052dc651b
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1028,4 +1028,9 @@ ssize_t fuse_passthrough_write_iter(struct kiocb *iocb, struct iov_iter *from);
  */
 unsigned int fuse_len_args(unsigned int numargs, struct fuse_arg *args);
 
+/**
+ * Get the next unique ID for a request
+ */
+u64 fuse_get_unique(struct fuse_iqueue *fiq);
+
 #endif /* _FS_FUSE_I_H */
