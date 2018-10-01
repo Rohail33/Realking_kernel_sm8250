b306d370c42717bb0bec69ace0c0bc8562a7f22b
Miklos Szeredi <mszeredi@redhat.com>
2018-10-01 10:07:04 +0200

    BACKPORT: fuse: allow using readdir cache
    The cache is only used if it's completed, not while it's still being
    filled; this constraint could be lifted later, if it turns out to be
    useful.
    
    Introduce state in struct fuse_file that indicates the position within the
    cache.  After a seek, reset the position to the beginning of the cache and
    search the cache for the current position.  If the current position is not
    found in the cache, then fall back to uncached readdir.
    
    It can also happen that page(s) disappear from the cache, in which case we
    must also fall back to uncached readdir.
    
    Change-Id: I4ec64ae5f6b48b6f2ca3bfe6b01747329d110844
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -62,6 +62,7 @@ struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 	}
 
 	INIT_LIST_HEAD(&ff->write_entry);
+	mutex_init(&ff->readdir.lock);
 	refcount_set(&ff->count, 1);
 	RB_CLEAR_NODE(&ff->polled_node);
 	init_waitqueue_head(&ff->poll_wait);
@@ -76,6 +77,7 @@ struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 void fuse_file_free(struct fuse_file *ff)
 {
 	fuse_request_free(ff->reserved_req);
+	mutex_destroy(&ff->readdir.lock);
 	kfree(ff);
 }
 
--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -175,6 +175,21 @@ struct fuse_file {
 	/** Entry on inode's write_files list */
 	struct list_head write_entry;
 
+	/* Readdir related */
+	struct {
+		/*
+		 * Protects below fields against (crazy) parallel readdir on
+		 * same open file.  Uncontended in the normal case.
+		 */
+		struct mutex lock;
+
+		/* Dir stream position */
+		loff_t pos;
+
+		/* Offset in cache */
+		loff_t cache_off;
+	} readdir;
+
 	/** Container for data related to the passthrough functionality */
         struct fuse_passthrough passthrough;
 
