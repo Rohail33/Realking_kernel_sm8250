271d633ac7f321dcc2e6e237ebc8c237631c3571
David Howells <dhowells@redhat.com>
2018-11-01 23:07:24 +0000

    UPSTREAM: selinux: Implement the new mount API LSM hooks
    Implement the new mount API LSM hooks for SELinux.  At some point the old
    hooks will need to be removed.
    
    Change-Id: I21f1eb1890ed78f0616b71bb6965e030e3f7b88f
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Paul Moore <paul@paul-moore.com>
    cc: Stephen Smalley <sds@tycho.nsa.gov>
    cc: selinux@tycho.nsa.gov
    cc: linux-security-module@vger.kernel.org
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -60,11 +60,11 @@
 #define SE_SBGENFS		0x0400
 #define SE_SBGENFS_XATTR	0x0800
 
-#define CONTEXT_STR	"context="
-#define FSCONTEXT_STR	"fscontext="
-#define ROOTCONTEXT_STR	"rootcontext="
-#define DEFCONTEXT_STR	"defcontext="
-#define LABELSUPP_STR "seclabel"
+#define CONTEXT_STR	"context"
+#define FSCONTEXT_STR	"fscontext"
+#define ROOTCONTEXT_STR	"rootcontext"
+#define DEFCONTEXT_STR	"defcontext"
+#define SECLABEL_STR "seclabel"
 
 struct netlbl_lsm_secattr;
 
