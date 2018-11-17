513b1abe41f6e766ef38613e8f4320685b6122a5
Al Viro <viro@zeniv.linux.org.uk>
2018-11-17 12:09:18 -0500

    UPSTREAM: new helper: security_sb_eat_lsm_opts()
    combination of alloc_secdata(), security_sb_copy_data(),
    security_sb_parse_opt_str() and free_secdata().
    
    Reviewed-by: David Howells <dhowells@redhat.com>
    Change-Id: Iec74464c3e3a89bdaf720519dd333a8c20248572
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/security.h
+++ include/linux/security.h
@@ -284,8 +284,6 @@ int security_sb_alloc(struct super_block *sb);
 void security_sb_free(struct super_block *sb);
 int security_sb_eat_lsm_opts(char *options, struct security_mnt_opts *opts);
 int security_sb_remount(struct super_block *sb, struct security_mnt_opts *opts);
-int security_sb_copy_data(char *orig, char *copy);
-int security_sb_remount(struct super_block *sb, struct security_mnt_opts *opts);
 int security_sb_kern_mount(struct super_block *sb, int flags,
 			   struct security_mnt_opts *opts);
 int security_sb_show_options(struct seq_file *m, struct super_block *sb);
@@ -598,11 +596,6 @@ static inline int security_sb_alloc(struct super_block *sb)
 
 static inline void security_sb_free(struct super_block *sb)
 { }
-
-static inline int security_sb_copy_data(char *orig, char *copy)
-{
-	return 0;
-}
 
 static inline int security_sb_eat_lsm_opts(char *options,
 					   struct security_mnt_opts *opts)
