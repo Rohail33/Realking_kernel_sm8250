70b6b22e85ba0de4f3f77a97d4eec8ce9713b9d7
Wilson Sung <wilsonsung@google.com>
2019-03-07 22:15:11 +0800

    msm: msm_bus: Fix error handling in msm_bus_device_probe
    The bus devices are not cleaned up by exit_device_probe.
    Add the msm_bus_device_remove to clean the bus devices.
    
    Bug: 127786140
    Change-Id: Id85de52abad9ee68507fa1b8ce0ad8c547fc1c6e
    Signed-off-by: Wilson Sung <wilsonsung@google.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_fabric_adhoc.c
+++ drivers/soc/qcom/msm_bus/msm_bus_fabric_adhoc.c
@@ -1136,7 +1136,6 @@ static int msm_bus_device_probe(struct platform_device *pdev)
 		ret = msm_bus_init_clk(node_dev, &pdata->info[i]);
 		if (ret) {
 			MSM_BUS_ERR("\n Failed to init bus clk. ret %d", ret);
-			msm_bus_device_remove(pdev);
 			goto exit_device_probe;
 		}
 		/*Is this a fabric device ?*/
@@ -1175,7 +1174,10 @@ static int msm_bus_device_probe(struct platform_device *pdev)
 
 	dev_info(&pdev->dev, "Bus scaling driver probe successful\n");
 
+	return 0;
+
 exit_device_probe:
+	msm_bus_device_remove(pdev);
 	return ret;
 }
 
