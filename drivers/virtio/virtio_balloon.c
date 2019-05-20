e29d8e691f8519258b2bba4cb82e858db1bb3a1a
Al Viro <viro@zeniv.linux.org.uk>
2019-05-20 13:44:56 +0100

    UPSTREAM: balloon: don't bother with dentry_operations
    Change-Id: I0f36c042185aa93885b5d75ef4b3d3f24b2d8a25
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/virtio/virtio_balloon.c
+++ drivers/virtio/virtio_balloon.c
@@ -513,11 +513,7 @@ static int virtballoon_migratepage(struct balloon_dev_info *vb_dev_info,
 static struct dentry *balloon_mount(struct file_system_type *fs_type,
 		int flags, const char *dev_name, void *data)
 {
-	static const struct dentry_operations ops = {
-		.d_dname = simple_dname,
-	};
-
-	return mount_pseudo(fs_type, NULL, &ops,
+	return mount_pseudo(fs_type, NULL, NULL,
 				BALLOON_KVM_MAGIC);
 }
 
