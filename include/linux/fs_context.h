092f8dbed51c11d453a47fca0bcdcbbe34744be1
Al Viro <viro@zeniv.linux.org.uk>
2019-06-01 20:48:55 -0400

    UPSTREAM: convenience helper get_tree_nodev()
    counterpart of mount_nodev().  Switch hugetlb and pseudo to it.
    
    Change-Id: I4b2d974b66226030f05c55dba3d68823b96a35b6
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/fs_context.h
+++ include/linux/fs_context.h
@@ -131,6 +131,9 @@ extern int vfs_get_super(struct fs_context *fc,
 			 enum vfs_get_super_keying keying,
 			 int (*fill_super)(struct super_block *sb,
 					   struct fs_context *fc));
+extern int get_tree_nodev(struct fs_context *fc,
+			 int (*fill_super)(struct super_block *sb,
+					   struct fs_context *fc));
 
 #define logfc(FC, FMT, ...) pr_notice(FMT, ## __VA_ARGS__)
 
