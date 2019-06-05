d0c99314b014710338d9d38b7f305826e2ae16e0
Dave Chinner <dchinner@redhat.com>
2019-06-05 08:04:47 -0700

    UPSTREAM: vfs: introduce generic_copy_file_range()
    Right now if vfs_copy_file_range() does not use any offload
    mechanism, it falls back to calling do_splice_direct(). This fails
    to do basic sanity checks on the files being copied. Before we
    start adding this necessarily functionality to the fallback path,
    separate it out into generic_copy_file_range().
    
    generic_copy_file_range() has the same prototype as
    ->copy_file_range() so that filesystems can use it in their custom
    ->copy_file_range() method if they so choose.
    
    Change-Id: I09f2c7002e568664bbf4c6410bc8cbc5346acf91
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1956,6 +1956,9 @@ extern int vfs_clone_file_range(struct file *file_in, loff_t pos_in,
 extern int vfs_dedupe_file_range_compare(struct inode *src, loff_t srcoff,
 					 struct inode *dest, loff_t destoff,
 					 loff_t len, bool *is_same);
+extern ssize_t generic_copy_file_range(struct file *file_in, loff_t pos_in,
+				       struct file *file_out, loff_t pos_out,
+				       size_t len, unsigned int flags);
 extern int vfs_dedupe_file_range(struct file *file,
 				 struct file_dedupe_range *same);
 extern int vfs_dedupe_file_range_one(struct file *src_file, loff_t src_pos,
