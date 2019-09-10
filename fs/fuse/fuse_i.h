08776286f87128a2a3448fde477445c758dd5061
Miklos Szeredi <mszeredi@redhat.com>
2019-09-10 15:04:10 +0200

    UPSTREAM: fuse: add simple background helper
    Create a helper named fuse_simple_background() that is similar to
    fuse_simple_request().  Unlike the latter, it returns immediately and calls
    the supplied 'end' callback when the reply is received.
    
    The supplied 'args' pointer is stored in 'fuse_req' which allows the
    callback to interpret the output arguments decoded from the reply.
    
    Change-Id: I9b2626d601d3b1508bbcc0ef99fc41203aeb2f8a
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -320,6 +320,7 @@ struct fuse_args {
 	bool page_replace:1;
 	struct fuse_in_arg in_args[3];
 	struct fuse_arg out_args[2];
+	void (*end)(struct fuse_conn *fc, struct fuse_args *args, int error);
 	struct path *canonical_path;
 };
 
@@ -400,6 +401,9 @@ struct fuse_req {
 
 	/** Entry on the interrupts list  */
 	struct list_head intr_entry;
+
+	/* Input/output arguments */
+	struct fuse_args *args;
 
 	/** refcount */
 	refcount_t count;
@@ -990,6 +994,8 @@ void fuse_request_send(struct fuse_conn *fc, struct fuse_req *req);
  * Simple request sending that does request allocation and freeing
  */
 ssize_t fuse_simple_request(struct fuse_conn *fc, struct fuse_args *args);
+int fuse_simple_background(struct fuse_conn *fc, struct fuse_args *args,
+			   gfp_t gfp_flags);
 
 /**
  * Send a request in the background
