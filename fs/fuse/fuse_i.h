69dce160861adbc58d4c563504e5c65e2f376908
Miklos Szeredi <mszeredi@redhat.com>
2019-09-10 15:04:08 +0200

    BACKPORT: fuse: flatten 'struct fuse_args'
    ...to make future expansion simpler.  The hiearachical structure is a
    historical thing that does not serve any practical purpose.
    
    The generated code is excatly the same before and after the patch.
    
    Change-Id: I71caae77a526b2385487da20200087d0917768e1
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -306,23 +306,14 @@ struct fuse_page_desc {
 };
 
 struct fuse_args {
-	struct {
-		struct {
-			uint32_t opcode;
-			uint64_t nodeid;
-		} h;
-		unsigned numargs;
-		struct fuse_in_arg args[3];
-
-	} in;
-	struct {
-		unsigned argvar:1;
-		unsigned numargs;
-		struct fuse_arg args[2];
-
-		/* Path used for completing d_canonical_path */
-		struct path *canonical_path;
-	} out;
+	uint32_t opcode;
+	uint64_t nodeid;
+	unsigned int in_numargs;
+	struct fuse_in_arg in_args[3];
+	unsigned int out_argvar:1;
+	unsigned int out_numargs;
+	struct fuse_arg out_args[2];
+	struct path *canonical_path;
 };
 
 #define FUSE_ARGS(args) struct fuse_args args = {}
