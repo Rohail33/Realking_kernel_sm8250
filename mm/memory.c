c185a7e21d6ee3ef754a35a0f8e09007fef31250
Wei Yang <richardw.yang@linux.intel.com>
2019-09-23 15:35:13 -0700

    mm: remove redundant assignment of entry
    Since ptent will not be changed after previous assignment of entry, it is
    not necessary to do the assignment again.
    
    Link: http://lkml.kernel.org/r/20190708082740.21111-1-richardw.yang@linux.intel.com
    Signed-off-by: Wei Yang <richardw.yang@linux.intel.com>
    Acked-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1166,7 +1166,6 @@ again:
 			continue;
 		}
 
-		entry = pte_to_swp_entry(ptent);
 		if (!non_swap_entry(entry)) {
 			/* Genuine swap entry, hence a private anon page */
 			if (!should_zap_cows(details))
