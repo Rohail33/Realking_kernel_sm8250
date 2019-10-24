003de8ca17b15ca6e5554c1eb54b2b195feca7d2
Minchan Kim <minchan@google.com>
2019-10-24 10:55:29 -0700

    Revert "mm: oom-kill: Add lmk_kill possible for ULMK"
    This reverts commit aa9e75a9ff9b3ed95641d704e6d907f706c672bc.
    
    Reason for revert: revet customized code
    Bug: 140544941
    Test: boot
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Martin Liu <liumartin@google.com>
    Change-Id: I1475e099c72dcdd33fc2497dd30aa51d07bfa73d
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4614,9 +4614,6 @@ retry:
 			     !(gfp_mask & __GFP_RETRY_MAYFAIL)))
 		goto nopage;
 
-	if (order <= PAGE_ALLOC_COSTLY_ORDER && should_ulmk_retry())
-		goto retry;
-
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
 		goto retry;
