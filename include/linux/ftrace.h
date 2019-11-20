2b34fb03c881520831399117c217b7bf15b998e8
Alexei Starovoitov <alexei.starovoitov@gmail.com>
2019-11-20 18:32:25 -0500

    UPSTREAM: ftrace: Return ENOTSUPP when DYNAMIC_FTRACE_WITH_DIRECT_CALLS is not configured
    When CONFIG_DYNAMIC_FTRACE_WITH_DIRECT_CALLS is not set it's best to
    have the stub functions return ENOTSUPP instead of ENODEV,
    otherwise ENODEV is a valid error when ip is incorrect which is
    indistinguishable from ftrace not compiled in.
    
    Link: http://lkml.kernel.org/r/CAADnVQ+OzTikM9EhrfsC7NFsVYhATW1SVHxK64w3xn9qpk81pg@mail.gmail.com
    
    Change-Id: I00f25fdf9736601679717ebc3fd69f86eaee87e3
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -262,16 +262,16 @@ struct ftrace_direct_func *ftrace_find_direct_func(unsigned long addr);
 #else
 static inline int register_ftrace_direct(unsigned long ip, unsigned long addr)
 {
-	return -ENODEV;
+	return -ENOTSUPP;
 }
 static inline int unregister_ftrace_direct(unsigned long ip, unsigned long addr)
 {
-	return -ENODEV;
+	return -ENOTSUPP;
 }
 static inline int modify_ftrace_direct(unsigned long ip,
 				       unsigned long old_addr, unsigned long new_addr)
 {
-	return -ENODEV;
+	return -ENOTSUPP;
 }
 static inline struct ftrace_direct_func *ftrace_find_direct_func(unsigned long addr)
 {
