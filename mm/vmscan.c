b28a06c63511d4f564496251ec5c33efc8d2fd7e
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-24 19:03:04 -0800

    simple_lmk: Use vmpressure notifier to trigger kills
    Using kswapd's scan depth to trigger task kills is inconsistent and
    unreliable. When memory pressure quickly spikes, the kswapd scan depth
    trigger fails to kick off Simple LMK fast enough, causing severe lag.
    Additionally, kswapd could stop scanning prematurely before reaching the
    desired scan depth to trigger Simple LMK, which could also cause stalls.
    
    To remedy this, use the vmpressure framework instead, since it provides
    more consistent and accurate readings on memory pressure. This is not
    very tunable though, so remove CONFIG_ANDROID_SIMPLE_LMK_AGGRESSION.
    Triggering Simple LMK to kill when the reported memory pressure is 100
    should yield good results on all setups.
    
    Change-Id: Ia51f38cb710754a2990dbfb87cd01c807c8f726c
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -50,7 +50,6 @@
 #include <linux/printk.h>
 #include <linux/dax.h>
 #include <linux/psi.h>
-#include <linux/simple_lmk.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -3638,7 +3637,6 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 		bool raise_priority = true;
 		bool ret;
 
-		simple_lmk_decide_reclaim(sc.priority);
 		sc.reclaim_idx = classzone_idx;
 
 		/*
