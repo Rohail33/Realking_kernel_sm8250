95c277f34115b3679f24cc49c8ade18d7413ad3a
Miklos Szeredi <mszeredi@redhat.com>
2020-05-19 14:50:37 +0200

    UPSTREAM: fuse: always allow query of st_dev
    Fuse mounts without "allow_other" are off-limits to all non-owners.  Yet it
    makes sense to allow querying st_dev on the root, since this value is
    provided by the kernel, not the userspace filesystem.
    
    Allow statx(2) with a zero request mask to succeed on a fuse mounts for all
    users.
    
    Reported-by: Nikolaus Rath <Nikolaus@rath.org>
    Change-Id: Ie7215acbfa5ee7cbfe512b3e12a00d1045f8fbc3
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1765,8 +1765,18 @@ static int fuse_getattr(const struct path *path, struct kstat *stat,
 	if (fuse_is_bad(inode))
 		return -EIO;
 
-	if (!fuse_allow_current_process(fc))
+	if (!fuse_allow_current_process(fc)) {
+		if (!request_mask) {
+			/*
+			 * If user explicitly requested *nothing* then don't
+			 * error out, but return st_dev only.
+			 */
+			stat->result_mask = 0;
+			stat->dev = inode->i_sb->s_dev;
+			return 0;
+		}
 		return -EACCES;
+	}
 
 	return fuse_update_get_attr(inode, NULL, stat, request_mask, flags);
 }
