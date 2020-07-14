c2984ac33588e5bbf164ec236b23090d1e967241
Miklos Szeredi <mszeredi@redhat.com>
2020-07-14 14:45:41 +0200

    UPSTREAM: fuse: reject options on reconfigure via fsconfig(2)
    Previous patch changed handling of remount/reconfigure to ignore all
    options, including those that are unknown to the fuse kernel fs.  This was
    done for backward compatibility, but this likely only affects the old
    mount(2) API.
    
    The new fsconfig(2) based reconfiguration could possibly be improved.  This
    would make the new API less of a drop in replacement for the old, OTOH this
    is a good chance to get rid of some weirdnesses in the old API.
    
    Several other behaviors might make sense:
    
     1) unknown options are rejected, known options are ignored
    
     2) unknown options are rejected, known options are rejected if the value
     is changed, allowed otherwise
    
     3) all options are rejected
    
    Prior to the backward compatibility fix to ignore all options all known
    options were accepted (1), even if they change the value of a mount
    parameter; fuse_reconfigure() does not look at the config values set by
    fuse_parse_param().
    
    To fix that we'd need to verify that the value provided is the same as set
    in the initial configuration (2).  The major drawback is that this is much
    more complex than just rejecting all attempts at changing options (3);
    i.e. all options signify initial configuration values and don't make sense
    on reconfigure.
    
    This patch opts for (3) with the rationale that no mount options are
    reconfigurable in fuse.
    
    Change-Id: I1eb25670fd437473a1a6c431fed1bb68a80c1046
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/linux/fs_context.h
+++ include/linux/fs_context.h
@@ -91,6 +91,7 @@ struct fs_context {
 	enum fs_context_purpose	purpose:8;
 	bool			need_free:1;	/* Need to call ops->free() */
 	bool			global:1;	/* Goes into &init_user_ns */
+	bool			oldapi:1;	/* Coming from mount(2) */
 };
 
 struct fs_context_operations {
