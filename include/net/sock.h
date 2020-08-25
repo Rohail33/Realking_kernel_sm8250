16dfec8c44a7590ccc87083f109fc7ab8a6398bc
KP Singh <kpsingh@google.com>
2020-08-25 20:29:13 +0200

    BACKPORT: bpf: Renames in preparation for bpf_local_storage
    A purely mechanical change to split the renaming from the actual
    generalization.
    
    Flags/consts:
    
      SK_STORAGE_CREATE_FLAG_MASK	BPF_LOCAL_STORAGE_CREATE_FLAG_MASK
      BPF_SK_STORAGE_CACHE_SIZE	BPF_LOCAL_STORAGE_CACHE_SIZE
      MAX_VALUE_SIZE		BPF_LOCAL_STORAGE_MAX_VALUE_SIZE
    
    Structs:
    
      bucket			bpf_local_storage_map_bucket
      bpf_sk_storage_map		bpf_local_storage_map
      bpf_sk_storage_data		bpf_local_storage_data
      bpf_sk_storage_elem		bpf_local_storage_elem
      bpf_sk_storage		bpf_local_storage
    
    The "sk" member in bpf_local_storage is also updated to "owner"
    in preparation for changing the type to void * in a subsequent patch.
    
    Functions:
    
      selem_linked_to_sk			selem_linked_to_storage
      selem_alloc				bpf_selem_alloc
      __selem_unlink_sk			bpf_selem_unlink_storage_nolock
      __selem_link_sk			bpf_selem_link_storage_nolock
      selem_unlink_sk			__bpf_selem_unlink_storage
      sk_storage_update			bpf_local_storage_update
      __sk_storage_lookup			bpf_local_storage_lookup
      bpf_sk_storage_map_free		bpf_local_storage_map_free
      bpf_sk_storage_map_alloc		bpf_local_storage_map_alloc
      bpf_sk_storage_map_alloc_check	bpf_local_storage_map_alloc_check
      bpf_sk_storage_map_check_btf		bpf_local_storage_map_check_btf
    
    Change-Id: Icdbdea3316ca2d2c2b76657515f1a444df8653d9
    Signed-off-by: KP Singh <kpsingh@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Link: https://lore.kernel.org/bpf/20200825182919.1118197-2-kpsingh@chromium.org

--- include/net/sock.h
+++ include/net/sock.h
@@ -237,7 +237,7 @@ struct sock_common {
 	/* public: */
 };
 
-struct bpf_sk_storage;
+struct bpf_local_storage;
 
 /**
   *	struct sock - network layer representation of sockets
@@ -500,7 +500,7 @@ struct sock {
 	void                    (*sk_destruct)(struct sock *sk);
 	struct sock_reuseport __rcu	*sk_reuseport_cb;
 #ifdef CONFIG_BPF_SYSCALL
-	struct bpf_sk_storage __rcu	*sk_bpf_storage;
+	struct bpf_local_storage __rcu	*sk_bpf_storage;
 #endif
 	struct rcu_head		sk_rcu;
 
