2c6aec1658deac1815213062dd0d3c071080134c
Qinglang Miao <miaoqinglang@huawei.com>
2020-09-16 14:21:39 +0800

    BACKPORT: thermal: core: remove unnecessary mutex_init()
    The mutex poweroff_lock is initialized statically. It is
    unnecessary to initialize by mutex_init().
    
    Signed-off-by: Qinglang Miao <miaoqinglang@huawei.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/20200916062139.191233-1-miaoqinglang@huawei.com
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1674,7 +1674,6 @@ static int __init thermal_init(void)
 {
 	int result;
 
-	mutex_init(&poweroff_lock);
 	thermal_passive_wq = alloc_workqueue("thermal_passive_wq",
 						WQ_HIGHPRI | WQ_UNBOUND
 						| WQ_FREEZABLE,
