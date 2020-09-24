78f330b3a89784380af98a8d00747d3aa122faa4
Christoph Hellwig <hch@lst.de>
2020-09-24 08:51:32 +0200

    BACKPORT: bdi: initialize ->ra_pages and ->io_pages in bdi_init
    Set up a readahead size by default, as very few users have a good
    reason to change it.  This means code, ecryptfs, and orangefs now
    set up the values while they were previously missing it, while ubifs,
    mtd and vboxsf manually set it to 0 to avoid readahead.
    
    Change-Id: I2394fea47e8685f26aee9cc52c72018cd2715741
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Acked-by: David Sterba <dsterba@suse.com> [btrfs]
    Acked-by: Richard Weinberger <richard@nod.at> [ubifs, mtd]
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/blk-core.c
+++ block/blk-core.c
@@ -1038,8 +1038,6 @@ struct request_queue *blk_alloc_queue_node(gfp_t gfp_mask, int node_id,
 	if (!q->stats)
 		goto fail_stats;
 
-	q->backing_dev_info->ra_pages = VM_READAHEAD_PAGES;
-	q->backing_dev_info->io_pages = VM_READAHEAD_PAGES;
 	q->backing_dev_info->capabilities = BDI_CAP_CGROUP_WRITEBACK;
 	q->backing_dev_info->name = "block";
 	q->node = node_id;
