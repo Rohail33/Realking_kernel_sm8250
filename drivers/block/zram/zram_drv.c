7f14a6ee78ee6c4c7da4a2f6242105c3bd01a107
Christoph Hellwig <hch@lst.de>
2020-11-16 15:57:09 +0100

    BACKPORT: zram: use set_capacity_and_notify
    Use set_capacity_and_notify to set the size of both the disk and block
    device.  This also gets the uevent notifications for the resize for free.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    [cyberknight777: backport to 4.14 & 4.19]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2207,7 +2207,7 @@ static void zram_reset_device(struct zram *zram)
 		return;
 	}
 
-	set_capacity(zram->disk, 0);
+	set_capacity_and_notify(zram->disk, 0);
 	part_stat_set_all(&zram->disk->part0, 0);
 
 	/* I/O operation under all of CPU are done so let's free */
@@ -2263,9 +2263,7 @@ static ssize_t disksize_store(struct device *dev,
 		zram->num_active_comps++;
 	}
 	zram->disksize = disksize;
-	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
-
-	revalidate_disk(zram->disk);
+	set_capacity_and_notify(zram->disk, zram->disksize >> SECTOR_SHIFT);
 	up_write(&zram->init_lock);
 
 	return len;
@@ -2313,7 +2311,6 @@ static ssize_t reset_store(struct device *dev,
 	/* Make sure all the pending I/O are finished */
 	sync_blockdev(bdev);
 	zram_reset_device(zram);
-	revalidate_disk(zram->disk);
 	bdput(bdev);
 
 	mutex_lock(&bdev->bd_mutex);
