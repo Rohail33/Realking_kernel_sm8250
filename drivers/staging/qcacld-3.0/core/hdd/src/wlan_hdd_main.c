bc4eae33c581cb614c602c5eb4826b3d3b6422a0
alk3pInjection <webmaster@raspii.tech>
2021-01-05 14:45:38 +0800

    qcacld-3.0: Fix build
    * also disable -Werror
    
    Change-Id: I799174195c3b74ee7ee89fe9abfb902a6304cade
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -15396,7 +15396,9 @@ int hdd_init(void)
 
 	hdd_trace_init();
 	hdd_register_debug_callback();
+#ifdef FEATURE_ROAM_DEBUG
 	wlan_roam_debug_init();
+#endif
 
 	return 0;
 }
@@ -15410,7 +15412,9 @@ int hdd_init(void)
  */
 void hdd_deinit(void)
 {
+#ifdef FEATURE_ROAM_DEBUG
 	wlan_roam_debug_deinit();
+#endif
 
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
 	wlan_logging_sock_deinit_svc();
