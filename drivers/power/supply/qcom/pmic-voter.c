c4ab13c4e51a1a0eb647b1cf64642a209e11ebe5
spakkkk <spakdroid@gmail.com>
2021-02-15 11:44:35 +0000

    drivers: power: disable some logs

--- drivers/power/supply/qcom/pmic-voter.c
+++ drivers/power/supply/qcom/pmic-voter.c
@@ -482,7 +482,7 @@ int vote(struct votable *votable, const char *client_str, bool enabled, int val)
 	if (!votable->voted_on
 			|| (effective_result != votable->effective_result)) {
 		if (strcmp(votable->name, "FG_WS") != 0) {
-				pr_info("%s: current vote is now %d voted by %s,%d, previous voted %d\n",
+				pr_debug("%s: current vote is now %d voted by %s,%d, previous voted %d\n",
 						votable->name, effective_result,
 						get_client_str(votable, effective_id),
 						effective_id, votable->effective_result);
