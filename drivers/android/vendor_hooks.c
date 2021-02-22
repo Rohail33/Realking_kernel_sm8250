e5901db61a20012b0221a0495d796aa6e7f4c471
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2021-02-22 16:35:21 +0800

    ANDROID: bpf: Add vendor hook
    Add vendor hook for bpf, so we can get memory type and
    use it to do memory type check for architecture
    dependent page table setting.
    
    Bug: 181639260
    
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    Change-Id: Icac325a040fb88c7f6b04b2409029b623bd8515f

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,9 +8,13 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
+#include <trace/hooks/memory.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
  */
-
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_memory_x);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_memory_nx);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_memory_ro);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_memory_rw);
