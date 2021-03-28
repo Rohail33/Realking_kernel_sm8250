44e6c31d368ddc4805e3efcebc436628f58c60ab
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2021-03-28 15:07:02 -0400

    techpack: display: Boost DDR to the max during fingerprint scans
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    [@0ctobot: Squash idkwhoiam322/weeb_kernel_oneplus_sm8150@1f0ecc2 with
    idkwhoiam322/weeb_kernel_oneplus_sm8150@a5f71e4. Adapt for 4.19]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Rohail33 <rohailyousafzai49@gmail.com>

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -28,6 +28,8 @@
 #include <drm/drm_flip_work.h>
 #include <linux/clk/qcom.h>
 
+#include <linux/devfreq_boost.h>
+
 #include "sde_kms.h"
 #include "sde_hw_lm.h"
 #include "sde_hw_ctl.h"
@@ -4757,6 +4759,10 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 
 	if (!is_dsi_panel(cstate->base.crtc))
 		return 0;
+
+	if (fppressed_index > 0 || fp_mode == 1) {
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 500);
+	}
 
 	if (oplus_dimlayer_bl_enable) {
 		int backlight = oplus_get_panel_brightness();
