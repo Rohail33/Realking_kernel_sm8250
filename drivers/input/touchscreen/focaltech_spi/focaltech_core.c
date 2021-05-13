591deb52afa0f6b5a3b774d20ef8e4dba13c94ae
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2021-05-13 15:07:37 -0500

    drivers: focaltech_spi: cleanup
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Little-W <1405481963@qq.com>

--- drivers/input/touchscreen/focaltech_spi/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_spi/focaltech_core.c
@@ -74,8 +74,6 @@ static int fts_ts_suspend(struct device *dev);
 static int fts_ts_resume(struct device *dev);
 
 #define LPM_EVENT_INPUT 0x1
-extern void lpm_disable_for_dev(bool on, char event_dev);
-extern void touch_irq_boost(void);
 
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 static void fts_read_palm_data(u8 reg_value);
@@ -418,7 +416,6 @@ void fts_release_all_finger(void)
 #endif
 	input_report_key(input_dev, BTN_TOUCH, 0);
 	input_sync(input_dev);
-	lpm_disable_for_dev(false, LPM_EVENT_INPUT);
 
 	fts_data->touchs = 0;
 	fts_data->key_state = 0;
@@ -539,7 +536,6 @@ static int fts_input_report_b(struct fts_ts_data *data)
 				FTS_DEBUG("[B]Points All Up!");
 			}
 			input_report_key(data->input_dev, BTN_TOUCH, 0);
-			lpm_disable_for_dev(false, LPM_EVENT_INPUT);
 		} else {
 			input_report_key(data->input_dev, BTN_TOUCH, 1);
 		}
@@ -751,7 +747,6 @@ static irqreturn_t fts_irq_handler(int irq, void *data)
 	int ret = 0;
 	struct fts_ts_data *ts_data = fts_data;
 
-	touch_irq_boost();
 	if ((ts_data->suspended) && (ts_data->pm_suspend)) {
 		ret = wait_for_completion_timeout(
 				  &ts_data->pm_completion,
@@ -761,12 +756,9 @@ static irqreturn_t fts_irq_handler(int irq, void *data)
 			return IRQ_HANDLED;
 		}
 	}
-#else
-	touch_irq_boost();
 #endif
 
 	pm_stay_awake(fts_data->dev);
-	lpm_disable_for_dev(true, LPM_EVENT_INPUT);
 	fts_irq_read_report();
 	pm_relax(fts_data->dev);
 	return IRQ_HANDLED;
