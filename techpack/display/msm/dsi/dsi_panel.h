5c24ca15f5743cffc7c4e57fd3e781e4e345bcb3
maniac103 <dannybaumann@web.de>
2021-08-01 08:21:53 +0100

    dsi_display: Add support for high brightness mode (HBM)
    @yarpiin: use MI HBM trigger to set brightness
    Change-Id: Id133e30c7bf645386053c42d4d121a053b03bac7
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -237,6 +237,8 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
+	int hbm_mode;
+
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -376,5 +378,8 @@ void dsi_panel_dealloc_cmd_packets(struct dsi_panel_cmd_set *set);
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
+
+
+int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
