3fe46e5e76af0db44b432aa7bd3837851c5ab7b7
psndna88 <psndna88@gmail.com>
2021-10-23 11:42:15 +0530

    binder: use dedicated highprio unbound workqueue
     Divyanshu-Modi:
      the original patch had a flaw which lead to
      a workqueue not being destructed.
    
    Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -99,6 +99,8 @@ static HLIST_HEAD(binder_dead_nodes);
 static DEFINE_SPINLOCK(binder_dead_nodes_lock);
 
 static struct dentry *binder_debugfs_dir_entry_root;
+static struct workqueue_struct *binder_deferred_workqueue;
+
 static struct dentry *binder_debugfs_dir_entry_proc;
 static struct dentry *binder_debugfs_dir_entry_proc_transaction;
 static atomic_t binder_last_id;
@@ -6181,7 +6183,7 @@ binder_defer_work(struct binder_proc *proc, enum binder_deferred_state defer)
 	if (hlist_unhashed(&proc->deferred_work_node)) {
 		hlist_add_head(&proc->deferred_work_node,
 				&binder_deferred_list);
-		schedule_work(&binder_deferred_work);
+		queue_work(binder_deferred_workqueue, &binder_deferred_work);
 	}
 	mutex_unlock(&binder_deferred_lock);
 }
@@ -6932,6 +6934,10 @@ static int __init binder_init(void)
 	if (ret)
 		return ret;
 
+	binder_deferred_workqueue = alloc_workqueue("binder", WQ_HIGHPRI | WQ_UNBOUND, 0);
+	if (!binder_deferred_workqueue)
+		goto err_workqueue_init_failed;
+
 	atomic_set(&binder_transaction_log.cur, ~0U);
 	atomic_set(&binder_transaction_log_failed.cur, ~0U);
 
@@ -7016,6 +7022,9 @@ err_init_binder_device_failed:
 err_alloc_device_names_failed:
 	debugfs_remove_recursive(binder_debugfs_dir_entry_root);
 	binder_alloc_shrinker_exit();
+
+err_workqueue_init_failed:
+	destroy_workqueue(binder_deferred_workqueue);
 
 	return ret;
 }
