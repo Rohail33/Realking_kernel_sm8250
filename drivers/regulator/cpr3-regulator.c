f03f342c2632723858ab6b645c011ea3a283046e
Nick Desaulniers <ndesaulniers@google.com>
2021-11-05 18:56:49 +0530

    Regulator: cpr3: fix -Wmisleading-indentation
    Fixes:
    drivers/regulator/cpr3-regulator.c:6491:3: error: misleading
    indentation; statement is not part of the previous 'if'
    [-Werror,-Wmisleading-indentation]
    if (rc)
    ^
    drivers/regulator/cpr3-regulator.c:6489:2: note: previous statement is here
    if (ctrl->ctrl_type == CPR_CTRL_TYPE_CPR4)
    ^
    
    Slightly more interesting case.  rc is initialized to zero, so it
    doesn't matter if this condition is within the parent condition or not.
    Do less work and move it into the condition as it was likely originally
    intended.
    
    Bug: 155426751
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Change-Id: Iac4bf04f5b9aec59dd74028970dc111e48fa9e2a
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/regulator/cpr3-regulator.c
+++ drivers/regulator/cpr3-regulator.c
@@ -6442,11 +6442,12 @@ int cpr3_regulator_unregister(struct cpr3_controller *ctrl)
 	cpr3_regulator_debugfs_ctrl_remove(ctrl);
 	mutex_unlock(&cpr3_controller_list_mutex);
 
-	if (ctrl->ctrl_type == CPR_CTRL_TYPE_CPR4)
+	if (ctrl->ctrl_type == CPR_CTRL_TYPE_CPR4) {
 		rc = cpr3_ctrl_clear_cpr4_config(ctrl);
 		if (rc)
 			cpr3_err(ctrl, "failed to clear CPR4 configuration,rc=%d\n",
 				rc);
+	}
 
 	cpr3_ctrl_loop_disable(ctrl);
 
