f63855c05748bc856f53ba47e782f18f8f2951bd
Paul Lawrence <paullawrence@google.com>
2022-01-12 19:29:56 +0000

    UPSTREAM: ANDROID: fuse-bpf: Support inotify
    Test: fuse_test, atest CtsOsTestCases:android.os.cts.FileObserverTest
    Bug: 202785178
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Signed-off-by: Alessio Balsini <balsini@google.com>
    Change-Id: I88719a8ab23c2042fb8f50462f023e247aa4b6c3

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -328,7 +328,19 @@ static void fuse_dentry_canonical_path(const struct path *path,
 	char *path_name;
 	int err;
 
-	path_name = (char *)__get_free_page(GFP_KERNEL);
+#ifdef CONFIG_FUSE_BPF
+	struct fuse_err_ret fer;
+
+	fer = fuse_bpf_backing(inode, struct fuse_dummy_io,
+						   fuse_canonical_path_initialize,
+						   fuse_canonical_path_backing,
+						   fuse_canonical_path_finalize, path,
+						   canonical_path);
+	if (fer.ret)
+		return;
+#endif
+
+	path_name = (char *)get_zeroed_page(GFP_KERNEL);
 	if (!path_name)
 		goto default_path;
 
