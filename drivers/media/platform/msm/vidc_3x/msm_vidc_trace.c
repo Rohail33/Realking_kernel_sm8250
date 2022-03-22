8f0ee0e75c5f53a780d9c2f53db1386783e129f4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-03-23 00:06:56 +0600

    vidc_3x: Instantiate tracepoints separately when CONFIG_TRACEPOINTS=y
    Tracepoints was defined in msm_vidc_debug which had been compiled out
    using CONFIG_DEBUG_KERNEL macro. So, if we replace the macro with
    CONFIG_TRACEPOINTS, then it will resolve the compilation error when
    Ftrace is enabled but will compile the other debugs alongside.
    Therefore, create a dedicated msm_vidc_trace.c for tracepoints
    and compile it when CONFIG_TRACEPOINTS is enabled.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

---
+++ drivers/media/platform/msm/vidc_3x/msm_vidc_trace.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2022, Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
+ */
+
+#include "msm_vidc_common.h"
+
+/* Instantiate tracepoints */
+#define CREATE_TRACE_POINTS
+#include "trace/events/msm_vidc.h"
