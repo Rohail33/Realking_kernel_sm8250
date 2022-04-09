c45bc4f2848185f0b2d85d38a52151e62cbc4880
Daniel Rosenberg <drosen@google.com>
2022-04-08 19:08:53 -0700

    UPSTREAM: ANDROID: fuse-bpf: Use cache and refcount
    Allocates the iocb we create for asynchronous IO from a cache instead of
    a regular kzalloc
    
    Test: generic/467 and fuse_test
    Bug: 217570523
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Change-Id: I27dcec89cd585835f6a8e80e1ae30c503f4038c8

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -1058,7 +1058,7 @@ int fuse_dev_init(void);
 void fuse_dev_cleanup(void);
 
 int fuse_ctl_init(void);
-void __exit fuse_ctl_cleanup(void);
+void fuse_ctl_cleanup(void);
 
 /**
  * Simple request sending that does request allocation and freeing
@@ -1782,6 +1782,9 @@ struct fuse_err_ret {
 	void *result;
 	bool ret;
 };
+
+int __init fuse_bpf_init(void);
+void __exit fuse_bpf_cleanup(void);
 
 /*
  * expression statement to wrap the backing filter logic
--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -1916,11 +1916,17 @@ static int __init fuse_init(void)
 	if (res)
 		goto err_sysfs_cleanup;
 
+	res = fuse_bpf_init();
+	if (res)
+		goto err_ctl_cleanup;
+
 	sanitize_global_limit(&max_user_bgreq);
 	sanitize_global_limit(&max_user_congthresh);
 
 	return 0;
 
+ err_ctl_cleanup:
+	fuse_ctl_cleanup();
  err_sysfs_cleanup:
 	fuse_sysfs_cleanup();
  err_dev_cleanup:
@@ -1938,6 +1944,7 @@ static void __exit fuse_exit(void)
 	fuse_ctl_cleanup();
 	fuse_sysfs_cleanup();
 	fuse_fs_cleanup();
+	fuse_bpf_cleanup();
 	fuse_dev_cleanup();
 }
 
