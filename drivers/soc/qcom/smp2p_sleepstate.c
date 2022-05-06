0784ba28305a4f98817f03f73c5a0fb6eeb35845
LowTension <zonarmr@gmail.com>
2022-05-06 00:05:18 -0500

    soc: qcom: smp2p_sleepstate: Reduce wakeup events to 100 ms

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -53,7 +53,11 @@ static int sleepstate_pm_notifier(struct notifier_block *nb,
 
 static irqreturn_t smp2p_sleepstate_handler(int irq, void *ctxt)
 {
+
 	__pm_wakeup_event(notify_ws, SMP2P_SLEEPSTATE_TIME);
+
+	__pm_wakeup_event(notify_ws, 100);
+
 	return IRQ_HANDLED;
 }
 
