f2d52e34e6e4a4b613de506476bc7b2baf8decfd
v4lkyr <valkyr23@gmail.com>
2022-05-08 01:11:01 +0300

    treewide: Avoid double unlikely() notation when using IS_ERR()
    The definition of IS_ERR() already applies the unlikely() notation
    when checking the error status of the passed pointer. For this
    reason there is no need to have the same notation outside of
    IS_ERR() itself.
    
    Clean up code by removing redundant notation.
    
    Inspired from Antonio Quartulli <a@unstable.cc> commit [1].
    But this time it's global, rather than drivers/net/wireguard scoped.
    
    [1]: https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet/commit/680755f3cf2f71eb9687451c7699df40e2e20a91
    
    Signed-off-by: v4lkyr <valkyr23@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -240,7 +240,7 @@ struct sock *__udp6_lib_lookup(struct net *net,
 						  exact_dif, hslot2,
 						  skb);
 		}
-		if (unlikely(IS_ERR(result)))
+		if (IS_ERR(result))
 			return NULL;
 		return result;
 	}
@@ -256,7 +256,7 @@ begin:
 						    saddr, sport);
 				result = reuseport_select_sock(sk, hash, skb,
 							sizeof(struct udphdr));
-				if (unlikely(IS_ERR(result)))
+				if (IS_ERR(result))
 					return NULL;
 				if (result)
 					return result;
