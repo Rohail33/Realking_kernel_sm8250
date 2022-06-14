0e87cfe5be7d287af2018f2f3605059fada8542e
Yuntao Wang <ytcoode@gmail.com>
2022-06-14 22:26:22 +0800

    UPSTREAM: bpf: Fix incorrect memory charge cost calculation in stack_map_alloc()
    commit b45043192b3e481304062938a6561da2ceea46a6 upstream.
    
    This is a backport of the original upstream patch for 5.4/5.10.
    
    The original upstream patch has been applied to 5.4/5.10 branches, which
    simply removed the line:
    
      cost += n_buckets * (value_size + sizeof(struct stack_map_bucket));
    
    This is correct for upstream branch but incorrect for 5.4/5.10 branches,
    as the 5.4/5.10 branches do not have the commit 370868107bf6 ("bpf:
    Eliminate rlimit-based memory accounting for stackmap maps"), so the
    bpf_map_charge_init() function has not been removed.
    
    Currently the bpf_map_charge_init() function in 5.4/5.10 branches takes a
    wrong memory charge cost, the
    
      attr->max_entries * (sizeof(struct stack_map_bucket) + (u64)value_size))
    
    part is missing, let's fix it.
    
    Cc: <stable@vger.kernel.org> # 5.4.y
    Cc: <stable@vger.kernel.org> # 5.10.y
    Change-Id: I91bcb932cab87a23f16a85db2e2f9269b5be8638
    Signed-off-by: Yuntao Wang <ytcoode@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/bpf/stackmap.c
+++ kernel/bpf/stackmap.c
@@ -123,7 +123,8 @@ static struct bpf_map *stack_map_alloc(union bpf_attr *attr)
 	n_buckets = roundup_pow_of_two(attr->max_entries);
 
 	cost = n_buckets * sizeof(struct stack_map_bucket *) + sizeof(*smap);
-	err = bpf_map_charge_init(&mem, cost);
+	err = bpf_map_charge_init(&mem, cost + attr->max_entries *
+			   (sizeof(struct stack_map_bucket) + (u64)value_size));
 	if (err)
 		return ERR_PTR(err);
 
