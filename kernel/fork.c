c0cd19a257ce57e8697c6c2f6b6512ad1f0323c3
Cyber Knight <cyberknight755@gmail.com>
2022-06-18 08:48:05 +0800

    kernel: Boost DDR bus and CPU freq according to set kernel profile
    - Boosting is done only when KProfiles is set to mode 2 or 3.
    - Otherwise boosting is skipped.
    - This patch is adapted to the latest implementation of KProfiles.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2341,6 +2341,7 @@ struct task_struct *fork_idle(int cpu)
 	return task;
 }
 
+extern int kp_active_mode(void);
 /*
  *  Ok, this is the main fork-routine.
  *
@@ -2360,9 +2361,17 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 100 ms when userspace launches an app */
+	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (is_zygote_pid(current->pid)) {
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	  /*
+	   * Dont boost CPU & DDR if battery saver profile is enabled
+	   * and boost CPU & DDR for 25ms if balanced profile is enabled
+	   */
+	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	  } else if (kp_active_mode() == 2) {
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+	  }
 	}
 
 	/*
