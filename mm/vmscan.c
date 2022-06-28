c304eb3472a05cce7906ae12635b91fb75bed0e7
Kazuki Hashimoto <kazukih@tuta.io>
2022-06-28 19:16:37 +0900

    Revert "mm: vmscan: support complete shrinker reclaim"
    This reverts commit 1b45604c21da15e188e5a4c65cc25033cbc19617.
    
    This makes the slab shrinker run longer than needed during kswapd
    reclaim, which causes microstalls.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -486,10 +486,6 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
 	long batch_size = shrinker->batch ? shrinker->batch
 					  : SHRINK_BATCH;
 	long scanned = 0, next_deferred;
-	long min_cache_size = batch_size;
-
-	if (current_is_kswapd())
-		min_cache_size = 0;
 
 	if (!(shrinker->flags & SHRINKER_NUMA_AWARE))
 		nid = 0;
@@ -569,7 +565,7 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
 	 * scanning at high prio and therefore should try to reclaim as much as
 	 * possible.
 	 */
-	while (total_scan > min_cache_size ||
+	while (total_scan >= batch_size ||
 	       total_scan >= freeable) {
 		unsigned long ret;
 		unsigned long nr_to_scan = min(batch_size, total_scan);
