23409c11b43dc2a6dbe6df4f77d2b74c6b0ec87c
Cyber Knight <cyberknight755@gmail.com>
2022-07-02 15:06:34 +0800

    simple_lmk: Expose paramters to sysfs
    - It would be very helpful to experiment with different values at runtime instead of rebuilding the kernel over and over again for a small value change.
    - Exposing these values allows us to tune them with kernel managers and such.
    - This may be dropped in the near future.
    - Inspired by commit [1].
    
    [1]: https://github.com/Neternels/android_kernel_xiaomi_mojito/commit/924d90caf5169afab7d0f04d5b4afdf8ad2cbfd0
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -16,13 +16,17 @@
 #include <uapi/linux/sched/types.h>
 
 /* The minimum number of pages to free per reclaim */
-#define MIN_FREE_PAGES (CONFIG_ANDROID_SIMPLE_LMK_MINFREE * SZ_1M / PAGE_SIZE)
+static unsigned short slmk_minfree __read_mostly = CONFIG_ANDROID_SIMPLE_LMK_MINFREE;
+module_param(slmk_minfree, short, 0644);
+#define MIN_FREE_PAGES (slmk_minfree * SZ_1M / PAGE_SIZE)
 
 /* Kill up to this many victims per reclaim */
 #define MAX_VICTIMS 1024
 
 /* Timeout in jiffies for each reclaim */
-#define RECLAIM_EXPIRES msecs_to_jiffies(CONFIG_ANDROID_SIMPLE_LMK_TIMEOUT_MSEC)
+static unsigned short slmk_timeout __read_mostly = CONFIG_ANDROID_SIMPLE_LMK_TIMEOUT_MSEC;
+module_param(slmk_timeout, short, 0644);
+#define RECLAIM_EXPIRES msecs_to_jiffies(slmk_timeout)
 
 struct victim_info {
 	struct task_struct *tsk;
