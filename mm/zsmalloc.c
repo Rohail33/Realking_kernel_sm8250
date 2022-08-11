62ed948e5fa6f5bdee45f3b3a1fa8391740712e8
Alexey Romanov <avromanov@sberdevices.ru>
2022-08-11 18:37:55 +0300

    BACKPORT: zsmalloc: remove unnecessary size_class NULL check
    pool->size_class array elements can't be NULL, so this check
    is not needed.
    
    In the whole code, we assign pool->size_class[i] values that are
    not NULL. Releasing memory for these values occurs in the
    zs_destroy_pool() function, which also releases and destroys the pool.
    
    In addition, in the zs_stats_size_show() and async_free_zspage(),
    with similar iterations over the array, we don't check it for NULL
    pointer.
    
    Link: https://lkml.kernel.org/r/20220811153755.16102-3-avromanov@sberdevices.ru
    Signed-off-by: Alexey Romanov <avromanov@sberdevices.ru>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [ Tashar02: Backport to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -2183,8 +2183,6 @@ unsigned long zs_compact(struct zs_pool *pool)
 
 	for (i = ZS_SIZE_CLASSES - 1; i >= 0; i--) {
 		class = pool->size_class[i];
-		if (!class)
-			continue;
 		if (class->index != i)
 			continue;
 		pages_freed += __zs_compact(pool, class);
@@ -2229,8 +2227,6 @@ static unsigned long zs_shrinker_count(struct shrinker *shrinker,
 
 	for (i = ZS_SIZE_CLASSES - 1; i >= 0; i--) {
 		class = pool->size_class[i];
-		if (!class)
-			continue;
 		if (class->index != i)
 			continue;
 
@@ -2389,9 +2385,6 @@ void zs_destroy_pool(struct zs_pool *pool)
 	for (i = 0; i < ZS_SIZE_CLASSES; i++) {
 		int fg;
 		struct size_class *class = pool->size_class[i];
-
-		if (!class)
-			continue;
 
 		if (class->index != i)
 			continue;
