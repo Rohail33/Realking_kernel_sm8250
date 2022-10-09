3a9d2799eb634f875a5fd10d3acc316c1ce7ff32
minaripenguin37 <alexfinhart@gmail.com>
2022-10-09 15:13:01 +0800

    sched: fair: Increase task migration cost
    * this increases cfs performance drastically when sched_autogroup is disabled.
    
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -144,7 +144,7 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
 unsigned int sysctl_sched_wakeup_granularity		= 1000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost	= 750000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 5000000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #ifdef CONFIG_SMP
