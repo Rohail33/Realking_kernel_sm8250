bc60a6b41f145504d5c92c180ffdd3c69a8117ae
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-23 21:47:12 +0530

    firmware: qcom: tz_log: Add support to get qsee_log from procfs
    Change-Id: Ib39bc672e81f7011728144932e4c4e00083dcb2e
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/firmware/qcom/tz_log.c
+++ drivers/firmware/qcom/tz_log.c
@@ -23,6 +23,10 @@
 #include <soc/qcom/qseecomi.h>
 #include <soc/qcom/qtee_shmbridge.h>
 
+#include <linux/proc_fs.h>
+#include <linux/wait.h>
+#include <linux/freezer.h>
+
 /* QSEE_LOG_BUF_SIZE = 32K */
 #define QSEE_LOG_BUF_SIZE 0x8000
 
@@ -469,6 +473,20 @@ static struct encrypted_log_info enc_tzlog_info;
 
 static int tzdbg_request_encrypted_log(dma_addr_t buf_paddr,
 				       size_t len, uint32_t log_id);
+
+static struct proc_dir_entry *g_proc_dir;
+static struct proc_dir_entry *p_qsee_log_dump_handler;
+static DECLARE_WAIT_QUEUE_HEAD(qseelog_waitqueue);
+static atomic_t qseelog_wait = ATOMIC_INIT(0);
+
+void read_qseelog_wakeup(void)
+{
+	if (atomic_read(&qseelog_wait)) {
+		atomic_set(&qseelog_wait, 0);
+		wake_up_all(&qseelog_waitqueue);
+	}
+}
+
 /*
  * Debugfs data structure and functions
  */
@@ -820,22 +838,34 @@ static int _disp_log_stats_v2(struct tzdbg_log_v2_t *log,
 	pr_debug("diag_buf wrap = %u, offset = %u\n",
 		log->log_pos.wrap, log->log_pos.offset);
 
-	while (log_start->offset == log->log_pos.offset) {
-		/*
-		 * No data in ring buffer,
-		 * so we'll hang around until something happens
-		 */
-		unsigned long t = msleep_interruptible(50);
-
-		if (t != 0) {
-			/* Some event woke us up, so let's quit */
-			return 0;
+	if (buf_idx == TZDBG_QSEE_LOG) {
+		while (log_start->offset == log->log_pos.offset) {
+			atomic_set(&qseelog_wait, 1);
+			if (wait_event_freezable(qseelog_waitqueue,
+			    atomic_read(&qseelog_wait) == 0)) {
+				/* Some event woke us up, so let's quit */
+				return 0;
+			}
 		}
+	} else {
+		while (log_start->offset == log->log_pos.offset) {
+			/*
+			 * No data in ring buffer,
+			 * so we'll hang around until something happens
+			 */
+			unsigned long t = msleep_interruptible(50);
 
-		if (buf_idx == TZDBG_LOG)
-			memcpy_fromio((void *)tzdbg.diag_buf, tzdbg.virt_iobase,
-						debug_rw_buf_size);
+			if (t != 0) {
+				/* Some event woke us up, so let's quit */
+				return 0;
+			}
 
+			if (buf_idx == TZDBG_LOG)
+				memcpy_fromio((void *)tzdbg.diag_buf,
+					      tzdbg.virt_iobase,
+					      debug_rw_buf_size);
+
+		}
 	}
 
 	max_len = (count > debug_rw_buf_size) ? debug_rw_buf_size : count;
@@ -1259,6 +1289,34 @@ static const struct file_operations tzdbg_fops = {
 	.open    = simple_open,
 };
 
+static ssize_t qsee_log_dump_procfs_read(struct file *file, char __user *buf,
+	size_t count, loff_t *offp)
+{
+	int len = 0;
+
+	len = _disp_qsee_log_stats(count);
+	*offp = 0;
+
+	if (len > count)
+		len = count;
+
+	return simple_read_from_buffer(buf, len, offp,
+				tzdbg.stat[TZDBG_QSEE_LOG].data, len);
+}
+
+
+static int qsee_log_dump_procfs_open(struct inode *inode, struct file *pfile)
+{
+	pfile->private_data = inode->i_private;
+	return 0;
+}
+
+const struct file_operations qsee_log_dump_proc_fops = {
+	.owner   = THIS_MODULE,
+	.read    = qsee_log_dump_procfs_read,
+	.open    = qsee_log_dump_procfs_open,
+};
+
 
 /*
  * Allocates log buffer from ION, registers the buffer at TZ
@@ -1435,6 +1493,22 @@ static int  tzdbgfs_init(struct platform_device *pdev)
 		}
 	}
 
+	g_proc_dir = proc_mkdir("tzdbg", 0);
+
+	if (g_proc_dir == 0) {
+		pr_err("Unable to mkdir /proc/tzdbg\n");
+		pr_err("%s: qsee log dump dirs in proc create dir failed !\n",
+								__func__);
+		rc = -ENOMEM;
+		goto err;
+	}
+
+	p_qsee_log_dump_handler = proc_create("qsee_log", 0, g_proc_dir,
+					      &qsee_log_dump_proc_fops);
+	if (p_qsee_log_dump_handler == NULL)
+		pr_err("%s: qsee log dump dirs in proc create failed !\n",
+								__func__);
+
 	platform_set_drvdata(pdev, dent_dir);
 	return 0;
 err:
@@ -1448,6 +1522,9 @@ static void tzdbgfs_exit(struct platform_device *pdev)
 	struct dentry *dent_dir;
 	dent_dir = platform_get_drvdata(pdev);
 	debugfs_remove_recursive(dent_dir);
+
+	if (p_qsee_log_dump_handler != NULL)
+		proc_remove(p_qsee_log_dump_handler);
 }
 
 static int __update_hypdbg_base(struct platform_device *pdev,
