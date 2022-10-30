8879a14bdd6681c59aad3f97cd14adbd7f3d2e6b
¨raystef66¨ <s.vanbarel@gmail.com>
2022-10-30 11:52:02 +0100

    Merge tag 'v4.19.262' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into base
    This is the 4.19.262 stable release

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -893,6 +893,8 @@ static const struct memdev {
 #endif
 	 [5] = { "zero", 0666, &zero_fops, 0 },
 	 [7] = { "full", 0666, &full_fops, 0 },
+         [8] = { "random", 0666, &random_fops, FMODE_NOWAIT },
+         [9] = { "urandom", 0666, &urandom_fops, FMODE_NOWAIT },
 	 [8] = { "random", 0666, &random_fops, 0 },
 	 [9] = { "urandom", 0666, &urandom_fops, 0 },
 #ifdef CONFIG_PRINTK
