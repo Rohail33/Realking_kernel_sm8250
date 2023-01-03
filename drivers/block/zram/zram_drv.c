1fe6b30e2d3ff8b78dcd380bead0db7059a63e28
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-01-03 12:01:19 +0900

    UPSTREAM:zram: correctly handle all next_arg() cases
    When supplied buffer does not have assignment sign next_arg() sets `val`
    pointer to NULL, so we cannot dereference it.  Add a NULL pointer test to
    handle `param` case, in addition to `*val` test, which handles cases when
    param has no value assigned to it: `param=`.
    
    Link: https://lkml.kernel.org/r/20230103030119.1496358-1-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1152,7 +1152,7 @@ static ssize_t recomp_algorithm_store(struct device *dev,
 	while (*args) {
 		args = next_arg(args, &param, &val);
 
-		if (!*val)
+		if (!val || !*val)
 			return -EINVAL;
 
 		if (!strcmp(param, "algo")) {
@@ -1833,7 +1833,7 @@ static ssize_t recompress_store(struct device *dev,
 	while (*args) {
 		args = next_arg(args, &param, &val);
 
-		if (!*val)
+		if (!val || !*val)
 			return -EINVAL;
 
 		if (!strcmp(param, "type")) {
