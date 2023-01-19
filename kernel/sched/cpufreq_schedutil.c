cda4208f7293116b0fc854ecbbfbcac5e26078ca
Rohail33 <rohailyousafzai49@gmail.com>
2023-01-19 20:04:08 +0000

    Fix the Missing Arguments in governors/teo.c and sched/cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -479,7 +479,7 @@ static unsigned long sugov_get_util(struct sugov_cpu *sg_cpu)
 
 unsigned long sched_cpu_util(int cpu)
 {
-	unsigned long max = arch_scale_cpu_capacity(cpu);
+	unsigned long max = arch_scale_cpu_capacity(NULL, cpu);
 
 	return schedutil_cpu_util(cpu, cpu_util_cfs(cpu_rq(cpu)), max, ENERGY_UTIL, NULL);
 }
