b24f34aa1a264720d589c6ef4e6076754a9dfb4c
John Galt <johngaltfirstrun@gmail.com>
2023-01-24 10:14:22 -0500

    [HACK] SSR: do not panic if subsys crashes during SSR
    Most drivers will continue trying to restart. Even if subsys stays
    offline, it is better for the user to find out and restart than
    panicking.

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1199,8 +1199,6 @@ static void __subsystem_restart_dev(struct subsys_device *dev)
 			track->p_state = SUBSYS_CRASHED;
 			__pm_stay_awake(dev->ssr_wlock);
 			queue_work(ssr_wq, &dev->work);
-		} else {
-			panic("Subsystem %s crashed during SSR!", name);
 		}
 	} else
 		WARN(dev->track.state == SUBSYS_OFFLINE,
