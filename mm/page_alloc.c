fb85c4d0f04b7c8abdc61a3d3e77a9ea6e15246f
Alexander Halbuer <halbuer@sra.uni-hannover.de>
2023-02-01 17:25:49 +0100

    mm: reduce lock contention of pcp buffer refill
    rmqueue_bulk() batches the allocation of multiple elements to refill the
    per-CPU buffers into a single hold of the zone lock.  Each element is
    allocated and checked using check_pcp_refill().  The check touches every
    related struct page which is especially expensive for higher order
    allocations (huge pages).
    
    This patch reduces the time holding the lock by moving the check out of
    the critical section similar to rmqueue_buddy() which allocates a single
    element.
    
    Measurements of parallel allocation-heavy workloads show a reduction of
    the average huge page allocation latency of 50 percent for two cores and
    nearly 90 percent for 24 cores.
    
    Link: https://lkml.kernel.org/r/20230201162549.68384-1-halbuer@sra.uni-hannover.de
    Signed-off-by: Alexander Halbuer <halbuer@sra.uni-hannover.de>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [Kazuki: Port to v5.4]
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    [Tashar02: Port to v4.19]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2696,6 +2696,8 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 {
 	const bool can_resched = !preempt_count() && !irqs_disabled();
 	int i, alloced = 0, last_mod = 0;
+	struct list_head *prev_tail = list->prev;
+	struct page *pos, *n;
 
 	spin_lock(&zone->lock);
 	for (i = 0; i < count; ++i) {
@@ -2726,9 +2728,6 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 			spin_lock(&zone->lock);
 		}
 
-		if (unlikely(check_pcp_refill(page)))
-			continue;
-
 		/*
 		 * Split buddy pages returned by expand() are received here in
 		 * physical page order. The page is added to the tail of
@@ -2740,7 +2739,6 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 		 * pages are ordered properly.
 		 */
 		list_add_tail(&page->lru, list);
-		alloced++;
 		if (is_migrate_cma(get_pcppage_migratetype(page)))
 			__mod_zone_page_state(zone, NR_FREE_CMA_PAGES,
 					      -(1 << order));
@@ -2754,6 +2752,22 @@ static int rmqueue_bulk(struct zone *zone, unsigned int order,
 	 */
 	__mod_zone_page_state(zone, NR_FREE_PAGES, -((i - last_mod) << order));
 	spin_unlock(&zone->lock);
+
+	/*
+	 * Pages are appended to the pcp list without checking to reduce the
+	 * time holding the zone lock. Checking the appended pages happens right
+	 * after the critical section while still holding the pcp lock.
+	 */
+	pos = list_first_entry(prev_tail, struct page, lru);
+	list_for_each_entry_safe_from(pos, n, list, lru) {
+		if (unlikely(check_pcp_refill(pos))) {
+			list_del(&pos->lru);
+			continue;
+		}
+
+		alloced++;
+	}
+
 	return alloced;
 }
 
