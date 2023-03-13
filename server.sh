ef21314b100df76b025a5561c438b59c28679f61
Rohail33 <rohailyousafzai49@gmail.com>
2023-03-13 20:15:51 +0000

    Build: Add tools

---
+++ server.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#set -e
+## Copy this script inside the kernel directory
+LINKER="lld"
+DIR=`readlink -f .`
+MAIN=`readlink -f ${DIR}/..`
+KERNEL_DEFCONFIG=alioth_defconfig
+export PATH="$MAIN/clang/bin:$PATH"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_COMPILER_STRING="$($MAIN/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
+if ! [ -d "$MAIN/clang" ]; then
+echo "ZYC clang not found! Cloning..."
+if ! git clone -q https://gitlab.com/ZyCromerZ/clang.git --depth=1 --single-branch $MAIN/clang; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+KERNEL_DIR=`pwd`
+ZIMAGE_DIR="$KERNEL_DIR/out/arch/arm64/boot"
+# Speed up build process
+MAKE="./makeparallel"
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$blue***********************************************"
+echo "          BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
+make $KERNEL_DEFCONFIG O=out CC=clang
+make -j$(nproc --all) O=out \
+                      CC=clang \
+                      ARCH=arm64 \
+                      LLVM=1 \
+                      LLVM_IAS=1 \
+                      LD_LIBRARY_PATH=lib \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
+
+TIME="$(date "+%Y%m%d-%H%M%S")"
+mkdir -p tmp
+cp -fp $ZIMAGE_DIR/Image.gz tmp
+cp -fp $ZIMAGE_DIR/dtbo.img tmp
+cp -fp $ZIMAGE_DIR/dtb tmp
+cp -rp ./anykernel/* tmp
+cd tmp
+7za a -mx9 tmp.zip *
+cd ..
+rm *.zip
+cp -fp tmp/tmp.zip RealKing-Alioth-MiUi-$TIME.zip
+rm -rf tmp
+echo $TIME
+
