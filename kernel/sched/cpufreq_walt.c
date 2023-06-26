31b713be757d7764396d24cb426793f303115cf3
ReaperSigma <vidium400@gmail.com>
2023-06-26 16:24:43 +0200

    SCHED: register correctly governors

--- kernel/sched/cpufreq_walt.c
+++ kernel/sched/cpufreq_walt.c
@@ -1196,4 +1196,8 @@ static struct cpufreq_governor walt_gov = {
 	.owner			= THIS_MODULE,
 };
 
-cpufreq_governor_init(walt_gov);
+static int __init walt_register(void)
+{
+	return cpufreq_register_governor(&walt_gov);
+}
+fs_initcall(walt_register);
