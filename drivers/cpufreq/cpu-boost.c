f3ae4cab354715901a850a36e568f5588323ead7
Rohail33 <rohailyousafzai49@gmail.com>
2023-07-23 22:09:14 +0200

    cpu-boost: Fix 'Use of Undeclared Variable'

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -394,7 +394,7 @@ void touch_irq_boost(void)
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if (now - last_input_time < MIN_INPUT_INTERVAL)
+	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
 	if (work_pending(&input_boost_work))
