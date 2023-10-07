c7ba942195c04e81dab753fcf5d330e1439a4df6
Mark-PK Tsai <mark-pk.tsai@mediatek.com>
2023-10-07 15:05:53 +0800

    zram: use copy_page for full page copy
    Some architectures, such as arm, have implemented optimized copy_page for
    full page copying.
    
    Replace the full page memcpy with copy_page to take advantage of the
    optimization.
    
    Link: https://lkml.kernel.org/r/20231007070554.8657-1-mark-pk.tsai@mediatek.com
    Signed-off-by: Mark-PK Tsai <mark-pk.tsai@mediatek.com>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Matthias Brugger <matthias.bgg@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: YJ Chiang <yj.chiang@mediatek.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1447,7 +1447,7 @@ static int zram_read_from_zspool(struct zram *zram, struct page *page,
 
 	if (size == PAGE_SIZE) {
 		dst = kmap_atomic(page);
-		memcpy(dst, src, PAGE_SIZE);
+		copy_page(dst, src);
 		kunmap_atomic(dst);
 		ret = 0;
 	} else {
