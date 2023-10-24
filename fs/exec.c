c97b0cc17daefcc0ead0da9a656068ecc16970be
Rohail33 <rohailyousafzai49@gmail.com>
2023-10-24 20:22:21 +0000

    Merge tag 'LA.UM.9.12.1.r1-06300-SMxx50.QSSI12.0' of https://git.codelinaro.org/clo/la/kernel/msm-4.19
    "LA.UM.9.12.1.r1-06300-SMxx50.QSSI12.0"
    
     Conflicts:
    	fs/exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1861,6 +1861,9 @@ static int __do_execve_file(int fd, struct filename *filename,
 		goto out_unmark;
 
 	bprm.argc = count(argv, MAX_ARG_STRINGS);
+	if (bprm.argc == 0)
+		pr_warn_once("process '%s' launched '%s' with NULL argv: empty string added\n",
+			     current->comm, bprm.filename);
 	if ((retval = bprm.argc) < 0)
 		goto out;
 
