b2b9d0ec7a28a3179ffc583d6b3ca1cb14d3fdec
Rohail33 <rohailyousafzai49@gmail.com>
2023-11-17 10:55:09 +0100

    drivers: rpmsg: fix use of undeclared identifier

--- drivers/rpmsg/qcom_glink_native.c
+++ drivers/rpmsg/qcom_glink_native.c
@@ -1718,8 +1718,6 @@ static void qcom_glink_rpdev_release(struct device *dev)
 {
 	struct rpmsg_device *rpdev = to_rpmsg_device(dev);
 
-	channel->rpdev = NULL;
-	kfree(rpdev->driver_override);
 	kfree(rpdev);
 }
 
