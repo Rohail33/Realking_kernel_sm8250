03bcac1c8ae9cc801643ed37379c65ec50e098bd
HELLBOY017 <abhaygill017@gmail.com>
2024-01-21 05:57:35 +0530

    treewide: Fix LLVM 18 warns

--- oplus/kernel/charger/oplus_vooc.c
+++ oplus/kernel/charger/oplus_vooc.c
@@ -106,7 +106,7 @@ bool oplus_vooc_get_fw_update_status(void)
 		return g_vooc_chip->mcu_update_ing;
 	}
 }
-void oplus_vooc_battery_update()
+void oplus_vooc_battery_update(void)
 {
 	struct oplus_vooc_chip *chip = g_vooc_chip;
 
@@ -1256,6 +1256,7 @@ static void oplus_vooc_fastchg_func(struct work_struct *work)
 	static bool normalchg_disabled = false;
 	int abnormal_dis_cnt = 0;
 	char buf[1] = { 0 };
+
 	/*
 	if (!g_adapter_chip) {
 		chg_err(" g_adapter_chip NULL\n");
@@ -2511,7 +2512,7 @@ void oplus_vooc_set_fastchg_to_warm_full_false(void)
 	}
 }
 
-bool oplus_vooc_get_fastchg_low_temp_full()
+bool oplus_vooc_get_fastchg_low_temp_full(void)
 {
 	if (!g_vooc_chip) {
 		return false;
