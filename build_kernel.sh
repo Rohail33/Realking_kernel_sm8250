9a35e6f11e2493e53bb6f7eaa1a34a5dbe7aaf2a
Rohail33 <rohailyousafzai49@gmail.com>
2024-02-26 20:18:06 +0000

    tree: Inline Kernel-Su

--- build_kernel.sh
+++ build_kernel.sh
@@ -43,8 +43,6 @@ revert_modifications() {
   git checkout arch/arm64/boot/dts/vendor/qcom/dsi-panel-* &>/dev/null
 }
 
-# kernel-SU add
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 
 echo -e "$blue***********************************************"
 echo "          BUILDING KERNEL          "
@@ -121,11 +119,6 @@ echo $TIME
 
 # Restore anykernel.sh
 mv -f anykernel/anykernel.sh.bak anykernel/anykernel.sh
-
-# Kernel-Su Remove from tree.
-git checkout drivers/Makefile &>/dev/null
-rm -rf KernelSU
-rm -rf drivers/kernelsu
 
 # Revert changes back to the original state
 revert_modifications
