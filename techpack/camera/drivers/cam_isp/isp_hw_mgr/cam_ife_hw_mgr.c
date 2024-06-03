53c6966a86e42b792356c8d780272a289824b572
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-06-03 18:45:38 +0530

    techpack: camera: Import minimal Xiaomi camera-dxo modifications
    Change-Id: I8840f62dbc312eb57950382c60602c6d206ddd4d
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/camera/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
+++ techpack/camera/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -3835,7 +3835,7 @@ static int cam_ife_mgr_config_hw(void *hw_mgr_priv,
 		for (i = 0; i < CAM_IFE_HW_CONFIG_WAIT_MAX_TRY; i++) {
 			rem_jiffies = wait_for_completion_timeout(
 				&ctx->config_done_complete,
-				msecs_to_jiffies(30));
+				msecs_to_jiffies(100));
 			if (rem_jiffies == 0) {
 				if (!cam_cdm_detect_hang_error(
 						ctx->cdm_handle)) {
@@ -4126,7 +4126,7 @@ static int cam_ife_mgr_stop_hw(void *hw_mgr_priv, void *stop_hw_args)
 	cam_ife_mgr_pause_hw(ctx);
 
 	rem_jiffies = wait_for_completion_timeout(&ctx->config_done_complete,
-		msecs_to_jiffies(10));
+		msecs_to_jiffies(300));
 	if (rem_jiffies == 0) {
 		CAM_WARN(CAM_ISP,
 			"config done completion timeout for last applied req_id=%llu rc=%d ctx_index %d",
--- techpack/camera/drivers/cam_req_mgr/cam_req_mgr_util.c
+++ techpack/camera/drivers/cam_req_mgr/cam_req_mgr_util.c
@@ -20,6 +20,7 @@
 static struct cam_req_mgr_util_hdl_tbl *hdl_tbl;
 static DEFINE_SPINLOCK(hdl_tbl_lock);
 
+static int hdl_count = 0;
 int cam_req_mgr_util_init(void)
 {
 	int rc = 0;
@@ -56,6 +57,8 @@ int cam_req_mgr_util_init(void)
 	hdl_tbl = hdl_tbl_local;
 	spin_unlock_bh(&hdl_tbl_lock);
 
+	hdl_count = 0;
+
 	return rc;
 
 bitmap_alloc_fail:
@@ -79,6 +82,7 @@ int cam_req_mgr_util_deinit(void)
 	hdl_tbl->bitmap = NULL;
 	kfree(hdl_tbl);
 	hdl_tbl = NULL;
+	hdl_count = 0;
 	spin_unlock_bh(&hdl_tbl_lock);
 
 	return 0;
@@ -105,6 +109,7 @@ int cam_req_mgr_util_free_hdls(void)
 		}
 	}
 	bitmap_zero(hdl_tbl->bitmap, CAM_REQ_MGR_MAX_HANDLES_V2);
+	hdl_count = 0;
 	spin_unlock_bh(&hdl_tbl_lock);
 
 	return 0;
@@ -116,11 +121,14 @@ static int32_t cam_get_free_handle_index(void)
 
 	idx = find_first_zero_bit(hdl_tbl->bitmap, hdl_tbl->bits);
 
-	if (idx >= CAM_REQ_MGR_MAX_HANDLES_V2 || idx < 0)
+	if (idx >= CAM_REQ_MGR_MAX_HANDLES_V2 || idx < 0) {
+		CAM_ERR(CAM_CRM, "wuchi Hdl tbl count is %d", hdl_count);
 		return -ENOSR;
+	}
 
 	set_bit(idx, hdl_tbl->bitmap);
 
+	hdl_count++;
 	return idx;
 }
 
@@ -406,6 +414,7 @@ static int cam_destroy_hdl(int32_t dev_hdl, int dev_hdl_type)
 	hdl_tbl->hdl[idx].ops   = NULL;
 	hdl_tbl->hdl[idx].priv  = NULL;
 	clear_bit(idx, hdl_tbl->bitmap);
+	hdl_count--;
 	spin_unlock_bh(&hdl_tbl_lock);
 
 	return 0;
