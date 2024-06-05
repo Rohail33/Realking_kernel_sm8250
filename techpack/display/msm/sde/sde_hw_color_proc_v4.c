781086a7e48f05e87766a1ebd9853719e74a07fb
kvsnr113 <kvsnrprojkt113@gmail.com>
2024-06-05 16:11:03 +0000

    KCAL: Increase default saturation and contrast by a bit
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- techpack/display/msm/sde/sde_hw_color_proc_v4.c
+++ techpack/display/msm/sde/sde_hw_color_proc_v4.c
@@ -12,9 +12,9 @@ static unsigned short kcal_red = 256;
 static unsigned short kcal_green = 256;
 static unsigned short kcal_blue = 256;
 static unsigned short kcal_hue = 0;
-static unsigned short kcal_sat = 255;
+static unsigned short kcal_sat = 265;
 static unsigned short kcal_val = 255;
-static unsigned short kcal_cont = 255;
+static unsigned short kcal_cont = 265;
 
 module_param(kcal_red, short, 0644);
 module_param(kcal_green, short, 0644);
