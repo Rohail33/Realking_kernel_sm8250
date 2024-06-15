WWHQ/kernel_xiaomi_sm8250

techpack: camera: Import Xiaomi camera-cas modifications
Change-Id: I830e6270f4ac59033d669fdc6f6b56e8c9be2262
Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

techpack/camera/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -4277,6 +4277,10 @@ static int cam_ife_mgr_start_hw(void *hw_mgr_priv, void *start_hw_args)
 	bool                              res_rdi_context_set = false;
 	uint32_t                          primary_rdi_src_res;
 	uint32_t                          primary_rdi_out_res;
+#ifdef CONFIG_BOARD_CAS
+	uint32_t                          last_rdi_src_res, last_rdi_res;
+	uint32_t                          last_rdi_out_res;
+#endif
 
 	primary_rdi_src_res = CAM_ISP_HW_VFE_IN_MAX;
 	primary_rdi_out_res = CAM_ISP_IFE_OUT_RES_MAX;
@@ -4379,6 +4383,31 @@ start_only:
 		goto cdm_streamoff;
 	}
 
+#ifdef CONFIG_BOARD_CAS
+	/* Find out last rdi out resource */
+	for (i = 0; i < CAM_IFE_HW_OUT_RES_MAX; i++) {
+		hw_mgr_res = &ctx->res_list_ife_out[i];
+		switch (hw_mgr_res->res_id) {
+		case CAM_ISP_IFE_OUT_RES_RDI_0:
+		case CAM_ISP_IFE_OUT_RES_RDI_1:
+		case CAM_ISP_IFE_OUT_RES_RDI_2:
+		case CAM_ISP_IFE_OUT_RES_RDI_3:
+			if (ctx->is_rdi_only_context) {
+				last_rdi_res = i;
+				last_rdi_out_res = hw_mgr_res->res_id;
+			}
+		default:
+			break;
+		}
+	}
+
+	if (ctx->is_rdi_only_context) {
+		hw_mgr_res = &ctx->res_list_ife_out[last_rdi_res];
+		hw_mgr_res->hw_res[0]->rdi_only_last_res =
+				ctx->is_rdi_only_context;
+	}
+#endif
+
 	CAM_DBG(CAM_ISP, "START IFE OUT ... in ctx id:%d",
 		ctx->ctx_index);
 	/* start the IFE out devices */
@@ -4412,6 +4441,12 @@ start_only:
 		primary_rdi_src_res =
 			cam_convert_rdi_out_res_id_to_src(primary_rdi_out_res);
 
+#ifdef CONFIG_BOARD_CAS
+	if (last_rdi_out_res < CAM_ISP_IFE_OUT_RES_MAX)
+		last_rdi_src_res =
+			cam_convert_rdi_out_res_id_to_src(last_rdi_out_res);
+#endif
+
 	CAM_DBG(CAM_ISP, "START IFE SRC ... in ctx id:%d",
 		ctx->ctx_index);
 	/* Start the IFE mux in devices */
@@ -4420,6 +4455,14 @@ start_only:
 			hw_mgr_res->hw_res[0]->rdi_only_ctx =
 				ctx->is_rdi_only_context;
 		}
+
+#ifdef CONFIG_BOARD_CAS
+		if (last_rdi_src_res == hw_mgr_res->res_id) {
+			hw_mgr_res->hw_res[0]->rdi_only_last_res =
+				ctx->is_rdi_only_context;
+		}
+#endif
+
 		rc = cam_ife_hw_mgr_start_hw_res(hw_mgr_res, ctx);
 		if (rc) {
 			CAM_ERR(CAM_ISP, "Can not start IFE Mux (%d)",

techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
@@ -1018,6 +1018,12 @@ int cam_ife_csid_cid_reserve(struct cam_ife_csid_hw *csid_hw,
 	if (csid_hw->csi2_reserve_cnt) {
 		/* current configure res type should match requested res type */
 		if (csid_hw->res_type != cid_reserv->in_port->res_type) {
+#ifdef CONFIG_BOARD_CAS
+			CAM_ERR(CAM_ISP, "[wrong res_type], CSID:%d, csid_hw->res_type:%d, cid_reserv->res_type:%d",
+                    csid_hw->hw_intf->hw_idx,
+                    csid_hw->res_type,
+                    cid_reserv->in_port->res_type);
+#endif
 			rc = -EINVAL;
 			goto end;
 		}
@@ -1046,6 +1052,15 @@ int cam_ife_csid_cid_reserve(struct cam_ife_csid_hw *csid_hw,
 			cid_reserv->in_port->test_pattern)) {
 
 			rc = -EINVAL;
+
+#ifdef CONFIG_BOARD_CAS
+			CAM_ERR(CAM_ISP,
+				"[wrong lane configuration]: CSID:%d res_sel:0x%x Lane type:%d lane_num:%d",
+				csid_hw->hw_intf->hw_idx,
+				csid_hw->csi2_rx_cfg.lane_cfg,
+				csid_hw->csi2_rx_cfg.lane_type,
+				csid_hw->csi2_rx_cfg.lane_num);
+#endif
 			goto end;
 		}
 	}

techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_bus/cam_vfe_bus_ver3.c
@@ -2173,8 +2173,18 @@ static int cam_vfe_bus_ver3_start_vfe_out(
 		return -EFAULT;
 	}
 
+#ifdef CONFIG_BOARD_CAS
+	CAM_INFO(CAM_ISP,
+		"VFE:%d out_type:0x%X RDI context %d for RUP ",
+			rsrc_data->common_data->core_index, rsrc_data->out_type, vfe_out->rdi_only_last_res);
+#endif
+
 	if ((common_data->is_lite || source_group > CAM_VFE_BUS_VER3_SRC_GRP_0)
+#ifdef CONFIG_BOARD_CAS
+		&& !vfe_out->rdi_only_last_res)
+#else
 		&& !vfe_out->rdi_only_ctx)
+#endif
 		goto end;
 
 	if (!common_data->rup_irq_handle[source_group]) {

techpack/camera/drivers/cam_sensor_module/cam_ois/cam_ois_core.c
@@ -927,6 +927,91 @@ static int cam_ois_get_data(struct cam_ois_ctrl_t *o_ctrl,
 
 	return rc;
 }
+
+#ifdef CONFIG_BOARD_CAS
+static int cam_tele_ois_get_data(struct cam_ois_ctrl_t *o_ctrl,
+				 struct cam_packet *csl_packet)
+{
+	struct cam_buf_io_cfg *io_cfg;
+	uint32_t i = 0;
+	int rc = 0;
+	uintptr_t buf_addr;
+	size_t buf_size;
+	uint8_t *read_buffer;
+	uint32_t num_data = sizeof(o_ctrl->ois_tele_data.data);
+	struct timespec64 ts64;
+	cycles_t t_now;
+	uint64_t boottime64;
+
+	memset(&o_ctrl->ois_tele_data, 0, sizeof(struct ois_tele_data_eis_t));
+	get_monotonic_boottime64(&ts64);
+	t_now = get_cycles();
+	boottime64 = (uint64_t)((ts64.tv_sec * 1000000000) + ts64.tv_nsec);
+
+	rc = camera_io_dev_read_seq(&(o_ctrl->io_master_info),
+				    OIS_TELE_DATA_ADDR,
+				    o_ctrl->ois_tele_data.data,
+				    CAMERA_SENSOR_I2C_TYPE_WORD,
+				    CAMERA_SENSOR_I2C_TYPE_BYTE, num_data);
+	o_ctrl->ois_tele_data.data_timestamp =
+		(uint64_t)(t_now * 10000 / 192); //< QTimer Freq = 19.2 MHz
+
+	if (rc < 0) {
+		CAM_ERR(CAM_OIS, "read failed");
+	} else {
+		CAM_DBG(CAM_OIS,
+			"ois_data count=%d,data_timestamp=%llu,boottime64=%llu,t_now=%llu",
+			o_ctrl->ois_tele_data.data[0],
+			o_ctrl->ois_tele_data.data_timestamp, boottime64,
+			t_now);
+	}
+
+	io_cfg = (struct cam_buf_io_cfg *)((uint8_t *)&csl_packet->payload +
+					   csl_packet->io_configs_offset);
+
+	CAM_DBG(CAM_OIS,
+		"number of IO configs: %d:", csl_packet->num_io_configs);
+
+	for (i = 0; i < csl_packet->num_io_configs; i++) {
+		CAM_DBG(CAM_OIS, "Direction: %d:", io_cfg->direction);
+		if (io_cfg->direction == CAM_BUF_OUTPUT) {
+			rc = cam_mem_get_cpu_buf(io_cfg->mem_handle[0],
+						 &buf_addr, &buf_size);
+			if (rc) {
+				CAM_ERR(CAM_OIS, "Fail in get buffer: %d", rc);
+				return rc;
+			}
+
+			CAM_DBG(CAM_OIS, "buf_addr : %pK, buf_size : %zu\n",
+				(void *)buf_addr, buf_size);
+
+			read_buffer = (uint8_t *)buf_addr;
+			if (!read_buffer) {
+				CAM_ERR(CAM_OIS, "invalid buffer to copy data");
+				rc = -EINVAL;
+				return rc;
+			}
+			read_buffer += io_cfg->offsets[0];
+
+			if (buf_size != sizeof(struct ois_tele_data_eis_t)) {
+				CAM_ERR(CAM_OIS,
+					"failed to copy, Invalid size");
+				rc = -EINVAL;
+				return rc;
+			}
+
+			CAM_DBG(CAM_OIS, "copy the data, len:%d", num_data);
+			memcpy(read_buffer, &o_ctrl->ois_tele_data,
+			       sizeof(struct ois_tele_data_eis_t));
+		} else {
+			CAM_ERR(CAM_OIS, "Invalid direction");
+			rc = -EINVAL;
+		}
+	}
+
+	return rc;
+}
+#endif // CONFIG_BOARD_CAS
 #endif
 
 /**
@@ -1334,6 +1419,24 @@ static int cam_ois_pkt_parse(struct cam_ois_ctrl_t *o_ctrl, void *arg)
 			return rc;
 		}
 		break;
+
+#ifdef CONFIG_BOARD_CAS
+	case CAM_OIS_PACKET_OPCODE_TELEOIS_GETDATA:
+		if (o_ctrl->cam_ois_state < CAM_OIS_CONFIG) {
+			rc = -EINVAL;
+			CAM_ERR(CAM_OIS,
+					"Not in right state to control OIS: %d",
+					o_ctrl->cam_ois_state);
+			return rc;
+		}
+		rc = cam_tele_ois_get_data(o_ctrl, csl_packet);
+		if (rc < 0) {
+			CAM_ERR(CAM_OIS,
+					"Fail ois_get_data: rc: %d", rc);
+			return rc;
+		}
+		break;
+#endif // CONFIG_BOARD_CAS
 #endif
 	default:
 		CAM_ERR(CAM_OIS, "Invalid Opcode: %d",

techpack/camera/drivers/cam_sensor_module/cam_ois/cam_ois_dev.h
@@ -27,6 +27,9 @@
 
 #define ENABLE_OIS_EIS
 #define OIS_DATA_ADDR 0x8A
+#ifdef CONFIG_BOARD_CAS
+#define OIS_TELE_DATA_ADDR 0x1100
+#endif
 
 enum cam_ois_state {
 	CAM_OIS_INIT,
@@ -139,6 +142,9 @@ struct cam_ois_ctrl_t {
 	uint8_t is_ois_pre_init;
 #ifdef ENABLE_OIS_EIS
 	struct ois_data_eis_t ois_data;
+#ifdef CONFIG_BOARD_CAS
+	struct ois_tele_data_eis_t ois_tele_data;
+#endif
 #endif
 };
 

techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_cmn_header.h
@@ -175,7 +175,12 @@ enum cam_ois_packet_opcodes {
 	CAM_OIS_PACKET_OPCODE_INIT,
 	CAM_OIS_PACKET_OPCODE_OIS_CONTROL,
 	CAM_OIS_PACKET_OPCODE_READ,
+#ifdef CONFIG_BOARD_CAS
+	CAM_OIS_PACKET_OPCODE_OIS_GETDATA,
+	CAM_OIS_PACKET_OPCODE_TELEOIS_GETDATA
+#else
 	CAM_OIS_PACKET_OPCODE_OIS_GETDATA
+#endif
 };
 
 enum msm_bus_perf_setting {

Rohail33/Realking_kernel_sm8250

techpack/camera/drivers/cam_isp/isp_hw_mgr/cam_ife_hw_mgr.c
@@ -4277,6 +4277,10 @@ static int cam_ife_mgr_start_hw(void *hw_mgr_priv, void *start_hw_args)
 	bool                              res_rdi_context_set = false;
 	uint32_t                          primary_rdi_src_res;
 	uint32_t                          primary_rdi_out_res;
+#ifdef CONFIG_CAMERA_CAS
+	uint32_t                          last_rdi_src_res, last_rdi_res;
+	uint32_t                          last_rdi_out_res;
+#endif
 
 	primary_rdi_src_res = CAM_ISP_HW_VFE_IN_MAX;
 	primary_rdi_out_res = CAM_ISP_IFE_OUT_RES_MAX;
@@ -4379,6 +4383,31 @@ start_only:
 		goto cdm_streamoff;
 	}
 
+#ifdef CONFIG_CAMERA_CAS
+	/* Find out last rdi out resource */
+	for (i = 0; i < CAM_IFE_HW_OUT_RES_MAX; i++) {
+		hw_mgr_res = &ctx->res_list_ife_out[i];
+		switch (hw_mgr_res->res_id) {
+		case CAM_ISP_IFE_OUT_RES_RDI_0:
+		case CAM_ISP_IFE_OUT_RES_RDI_1:
+		case CAM_ISP_IFE_OUT_RES_RDI_2:
+		case CAM_ISP_IFE_OUT_RES_RDI_3:
+			if (ctx->is_rdi_only_context) {
+				last_rdi_res = i;
+				last_rdi_out_res = hw_mgr_res->res_id;
+			}
+		default:
+			break;
+		}
+	}
+
+	if (ctx->is_rdi_only_context) {
+		hw_mgr_res = &ctx->res_list_ife_out[last_rdi_res];
+		hw_mgr_res->hw_res[0]->rdi_only_last_res =
+				ctx->is_rdi_only_context;
+	}
+#endif
+
 	CAM_DBG(CAM_ISP, "START IFE OUT ... in ctx id:%d",
 		ctx->ctx_index);
 	/* start the IFE out devices */
@@ -4412,6 +4441,12 @@ start_only:
 		primary_rdi_src_res =
 			cam_convert_rdi_out_res_id_to_src(primary_rdi_out_res);
 
+#ifdef CONFIG_CAMERA_CAS
+	if (last_rdi_out_res < CAM_ISP_IFE_OUT_RES_MAX)
+		last_rdi_src_res =
+			cam_convert_rdi_out_res_id_to_src(last_rdi_out_res);
+#endif
+
 	CAM_DBG(CAM_ISP, "START IFE SRC ... in ctx id:%d",
 		ctx->ctx_index);
 	/* Start the IFE mux in devices */
@@ -4420,6 +4455,14 @@ start_only:
 			hw_mgr_res->hw_res[0]->rdi_only_ctx =
 				ctx->is_rdi_only_context;
 		}
+
+#ifdef CONFIG_CAMERA_CAS
+		if (last_rdi_src_res == hw_mgr_res->res_id) {
+			hw_mgr_res->hw_res[0]->rdi_only_last_res =
+				ctx->is_rdi_only_context;
+		}
+#endif
+
 		rc = cam_ife_hw_mgr_start_hw_res(hw_mgr_res, ctx);
 		if (rc) {
 			CAM_ERR(CAM_ISP, "Can not start IFE Mux (%d)",

techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/ife_csid_hw/cam_ife_csid_core.c
@@ -1018,6 +1018,12 @@ int cam_ife_csid_cid_reserve(struct cam_ife_csid_hw *csid_hw,
 	if (csid_hw->csi2_reserve_cnt) {
 		/* current configure res type should match requested res type */
 		if (csid_hw->res_type != cid_reserv->in_port->res_type) {
+#ifdef CONFIG_CAMERA_CAS
+			CAM_ERR(CAM_ISP, "[wrong res_type], CSID:%d, csid_hw->res_type:%d, cid_reserv->res_type:%d",
+                    csid_hw->hw_intf->hw_idx,
+                    csid_hw->res_type,
+                    cid_reserv->in_port->res_type);
+#endif
 			rc = -EINVAL;
 			goto end;
 		}
@@ -1046,6 +1052,15 @@ int cam_ife_csid_cid_reserve(struct cam_ife_csid_hw *csid_hw,
 			cid_reserv->in_port->test_pattern)) {
 
 			rc = -EINVAL;
+
+#ifdef CONFIG_CAMERA_CAS
+			CAM_ERR(CAM_ISP,
+				"[wrong lane configuration]: CSID:%d res_sel:0x%x Lane type:%d lane_num:%d",
+				csid_hw->hw_intf->hw_idx,
+				csid_hw->csi2_rx_cfg.lane_cfg,
+				csid_hw->csi2_rx_cfg.lane_type,
+				csid_hw->csi2_rx_cfg.lane_num);
+#endif
 			goto end;
 		}
 	}

techpack/camera/drivers/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/vfe_bus/cam_vfe_bus_ver3.c
@@ -2173,8 +2173,18 @@ static int cam_vfe_bus_ver3_start_vfe_out(
 		return -EFAULT;
 	}
 
+#ifdef CONFIG_CAMERA_CAS
+	CAM_INFO(CAM_ISP,
+		"VFE:%d out_type:0x%X RDI context %d for RUP ",
+			rsrc_data->common_data->core_index, rsrc_data->out_type, vfe_out->rdi_only_last_res);
+#endif
+
 	if ((common_data->is_lite || source_group > CAM_VFE_BUS_VER3_SRC_GRP_0)
+#ifdef CONFIG_CAMERA_CAS
+		&& !vfe_out->rdi_only_last_res)
+#else
 		&& !vfe_out->rdi_only_ctx)
+#endif
 		goto end;
 
 	if (!common_data->rup_irq_handle[source_group]) {

techpack/camera/drivers/cam_sensor_module/cam_ois/cam_ois_core.c
@@ -1145,6 +1145,91 @@ static int cam_ois_get_data(struct cam_ois_ctrl_t *o_ctrl,
 
 	return rc;
 }
+
+#ifdef CONFIG_CAMERA_CAS
+static int cam_tele_ois_get_data(struct cam_ois_ctrl_t *o_ctrl,
+				 struct cam_packet *csl_packet)
+{
+	struct cam_buf_io_cfg *io_cfg;
+	uint32_t i = 0;
+	int rc = 0;
+	uintptr_t buf_addr;
+	size_t buf_size;
+	uint8_t *read_buffer;
+	uint32_t num_data = sizeof(o_ctrl->ois_tele_data.data);
+	struct timespec64 ts64;
+	cycles_t t_now;
+	uint64_t boottime64;
+
+	memset(&o_ctrl->ois_tele_data, 0, sizeof(struct ois_tele_data_eis_t));
+	get_monotonic_boottime64(&ts64);
+	t_now = get_cycles();
+	boottime64 = (uint64_t)((ts64.tv_sec * 1000000000) + ts64.tv_nsec);
+
+	rc = camera_io_dev_read_seq(&(o_ctrl->io_master_info),
+				    OIS_TELE_DATA_ADDR,
+				    o_ctrl->ois_tele_data.data,
+				    CAMERA_SENSOR_I2C_TYPE_WORD,
+				    CAMERA_SENSOR_I2C_TYPE_BYTE, num_data);
+	o_ctrl->ois_tele_data.data_timestamp =
+		(uint64_t)(t_now * 10000 / 192); //< QTimer Freq = 19.2 MHz
+
+	if (rc < 0) {
+		CAM_ERR(CAM_OIS, "read failed");
+	} else {
+		CAM_DBG(CAM_OIS,
+			"ois_data count=%d,data_timestamp=%llu,boottime64=%llu,t_now=%llu",
+			o_ctrl->ois_tele_data.data[0],
+			o_ctrl->ois_tele_data.data_timestamp, boottime64,
+			t_now);
+	}
+
+	io_cfg = (struct cam_buf_io_cfg *)((uint8_t *)&csl_packet->payload +
+					   csl_packet->io_configs_offset);
+
+	CAM_DBG(CAM_OIS,
+		"number of IO configs: %d:", csl_packet->num_io_configs);
+
+	for (i = 0; i < csl_packet->num_io_configs; i++) {
+		CAM_DBG(CAM_OIS, "Direction: %d:", io_cfg->direction);
+		if (io_cfg->direction == CAM_BUF_OUTPUT) {
+			rc = cam_mem_get_cpu_buf(io_cfg->mem_handle[0],
+						 &buf_addr, &buf_size);
+			if (rc) {
+				CAM_ERR(CAM_OIS, "Fail in get buffer: %d", rc);
+				return rc;
+			}
+
+			CAM_DBG(CAM_OIS, "buf_addr : %pK, buf_size : %zu\n",
+				(void *)buf_addr, buf_size);
+
+			read_buffer = (uint8_t *)buf_addr;
+			if (!read_buffer) {
+				CAM_ERR(CAM_OIS, "invalid buffer to copy data");
+				rc = -EINVAL;
+				return rc;
+			}
+			read_buffer += io_cfg->offsets[0];
+
+			if (buf_size != sizeof(struct ois_tele_data_eis_t)) {
+				CAM_ERR(CAM_OIS,
+					"failed to copy, Invalid size");
+				rc = -EINVAL;
+				return rc;
+			}
+
+			CAM_DBG(CAM_OIS, "copy the data, len:%d", num_data);
+			memcpy(read_buffer, &o_ctrl->ois_tele_data,
+			       sizeof(struct ois_tele_data_eis_t));
+		} else {
+			CAM_ERR(CAM_OIS, "Invalid direction");
+			rc = -EINVAL;
+		}
+	}
+
+	return rc;
+}
+#endif // CONFIG_CAMERA_CAS
 #endif
 
 /**
@@ -1574,6 +1659,24 @@ static int cam_ois_pkt_parse(struct cam_ois_ctrl_t *o_ctrl, void *arg)
 			return rc;
 		}
 		break;
+
+#ifdef CONFIG_CAMERA_CAS
+	case CAM_OIS_PACKET_OPCODE_TELEOIS_GETDATA:
+		if (o_ctrl->cam_ois_state < CAM_OIS_CONFIG) {
+			rc = -EINVAL;
+			CAM_ERR(CAM_OIS,
+					"Not in right state to control OIS: %d",
+					o_ctrl->cam_ois_state);
+			return rc;
+		}
+		rc = cam_tele_ois_get_data(o_ctrl, csl_packet);
+		if (rc < 0) {
+			CAM_ERR(CAM_OIS,
+					"Fail ois_get_data: rc: %d", rc);
+			return rc;
+		}
+		break;
+#endif // CONFIG_CAMERA_CAS
 #endif
 	default:
 		CAM_ERR(CAM_OIS, "Invalid Opcode: %d",

techpack/camera/drivers/cam_sensor_module/cam_ois/cam_ois_dev.h
@@ -27,6 +27,9 @@
 
 #define ENABLE_OIS_EIS
 #define OIS_DATA_ADDR 0x8A
+#ifdef CONFIG_CAMERA_CAS
+#define OIS_TELE_DATA_ADDR 0x1100
+#endif
 
 enum cam_ois_state {
 	CAM_OIS_INIT,
@@ -139,6 +142,9 @@ struct cam_ois_ctrl_t {
 	uint8_t is_ois_pre_init;
 #ifdef ENABLE_OIS_EIS
 	struct ois_data_eis_t ois_data;
+#ifdef CONFIG_CAMERA_CAS
+	struct ois_tele_data_eis_t ois_tele_data;
+#endif
 #endif
 };
 

techpack/camera/drivers/cam_sensor_module/cam_sensor_utils/cam_sensor_cmn_header.h
@@ -175,7 +175,12 @@ enum cam_ois_packet_opcodes {
 	CAM_OIS_PACKET_OPCODE_INIT,
 	CAM_OIS_PACKET_OPCODE_OIS_CONTROL,
 	CAM_OIS_PACKET_OPCODE_READ,
+#ifdef CONFIG_CAMERA_CAS
+	CAM_OIS_PACKET_OPCODE_OIS_GETDATA,
+	CAM_OIS_PACKET_OPCODE_TELEOIS_GETDATA
+#else
 	CAM_OIS_PACKET_OPCODE_OIS_GETDATA
+#endif
 };
 
 enum msm_bus_perf_setting {