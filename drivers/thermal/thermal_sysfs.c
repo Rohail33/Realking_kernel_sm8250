b06446b6c586db7e2f92ace02377637e81b60f36
hridaya <hridaya@pixelos.net>
2024-10-01 21:15:20 +0300

    thermal_sysfs: Use vmalloc for cooling_device_stats_setup() cooling_device_stats_setup() will kzalloc() max_brightness * max_brightness * 4 bytes memory for trans_table, if max_brightness is too big, kzalloc() will panic with following call trace log.
    [    1.470140] WARNING: CPU: 0 PID: 1 at mm/page_alloc.c:4885 __alloc_pages_nodemask+0x248/0xffc
    [    1.470145] Modules linked in:
    [    1.470155] CPU: 0 PID: 1 Comm: swapper/0 Tainted: G S                4.19.295-perf-@2ce19c35 #1
    [    1.470160] Hardware name: Qualcomm Technologies, Inc. Gauguin (DT)
    [    1.470165] pstate: 20c00005 (nzCv daif +PAN +UAO)
    [    1.470171] pc : __alloc_pages_nodemask+0x248/0xffc
    [    1.470178] lr : kmalloc_order_trace+0x40/0x128
    [    1.470182] sp : ffffff800805b2b0
    [    1.470185] x29: ffffff800805b3d0 x28: ffffffe102954000
    [    1.470191] x27: ffffffe10294d000 x26: ffffffe10294e720
    [    1.470196] x25: ffffffe10294e000 x24: 000000000000000e
    [    1.470201] x23: 0000000000000000 x22: ffffffe2fff19a98
    [    1.470206] x21: ffffffa4b2c7811c x20: 0000000004008030
    [    1.470211] x19: 00000000006080c0 x18: 000000000007f150
    [    1.470216] x17: 000000000007f158 x16: 00000000000000c4
    [    1.470222] x15: ffffffa4b40916b8 x14: 0000000000003937
    [    1.470227] x13: ffffff800805b282 x12: 0000000000000000
    [    1.470232] x11: 0101010101010101 x10: 0000000000000030
    [    1.470237] x9 : ba0c8a977203c100 x8 : 000000000060c0c0
    [    1.470242] x7 : 622d306c656e6170 x6 : ffffffe2efb94814
    [    1.470247] x5 : 8080808080808080 x4 : 0000000000000000
    [    1.470252] x3 : 0000000000000000 x2 : 0000000000000000
    [    1.470257] x1 : 000000000000000f x0 : 000000000060c0c0
    [    1.470262] Call trace:
    [    1.470268] __alloc_pages_nodemask+0x248/0xffc
    [    1.470273] kmalloc_order_trace+0x40/0x128
    [    1.470279] __kmalloc+0x290/0x330
    [    1.470287] thermal_cooling_device_setup_sysfs+0x68/0xdc
    [    1.470293] __thermal_cooling_device_register.llvm.10969504886130080893+0x120/0x258
    [    1.470299] backlight_device_register+0x174/0x208
    [    1.470307] sde_connector_init+0x250/0xb4c
    [    1.470312] sde_kms_hw_init+0x1000/0x1d94
    [    1.470318] msm_drm_bind+0x53c/0x8e0
    [    1.470324] try_to_bring_up_master+0x13c/0x184
    [    1.470329] component_master_add_with_match+0xfc/0x1a8
    [    1.470334] msm_pdev_probe+0x100/0x33c
    [    1.470340] platform_drv_probe+0x7c/0xb4
    [    1.470346] really_probe+0x274/0x2f8
    [    1.470350] driver_probe_device+0x60/0xf8
    [    1.470355] __driver_attach.llvm.16524695203660845504+0xe0/0x118
    [    1.470360] bus_add_driver+0x184/0x284
    [    1.470365] driver_register+0x74/0x10c
    [    1.470370] __platform_driver_register+0x40/0x48
    [    1.470378] msm_drm_register+0x40/0x48
    [    1.470384] do_one_initcall+0x188/0x2e4
    [    1.470392] do_initcall_level+0x138/0x158
    [    1.470397] do_basic_setup+0x30/0x48
    [    1.470402] kernel_init_freeable+0xb8/0x134
    [    1.470409] kernel_init+0x14/0x29c
    [    1.470415] ret_from_fork+0x10/0x18
    [    1.470422] ---[ end trace 7b5439d9338324a0 ]---
    
    Change-Id: Ifd0fb1eb26e44005a997bf9a229fa1dcd75ee1bf
    Signed-off-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- drivers/thermal/thermal_sysfs.c
+++ drivers/thermal/thermal_sysfs.c
@@ -18,6 +18,7 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/jiffies.h>
+#include <linux/vmalloc.h>
 
 #include "thermal_core.h"
 
@@ -984,7 +985,7 @@ static void cooling_device_stats_setup(struct thermal_cooling_device *cdev)
 	var += sizeof(*stats->time_in_state) * states;
 	var += sizeof(*stats->trans_table) * states * states;
 
-	stats = kzalloc(var, GFP_KERNEL);
+        stats = vzalloc(var);
 	if (!stats)
 		goto out;
 
@@ -1006,7 +1007,7 @@ out:
 
 static void cooling_device_stats_destroy(struct thermal_cooling_device *cdev)
 {
-	kfree(cdev->stats);
+	vfree(cdev->stats);
 	cdev->stats = NULL;
 }
 
