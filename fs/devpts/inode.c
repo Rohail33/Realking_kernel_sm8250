f463d5c382ba3e417108309111e1ca26a8d4d76a
Rohail33 <rohailyousafzai49@gmail.com>
2025-01-28 20:53:42 +0100

    fs: devpts: fix error implicit declaration of function 'ksu_handle_devpts'

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -612,9 +612,6 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_devpts(dentry->d_inode);
-#endif
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
