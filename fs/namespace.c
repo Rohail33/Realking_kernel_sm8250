6269269c10f2ae7170d31ddfd1a58d7b634515e8
Thomas Turner <thomas_turner@talktalk.net>
2025-08-28 20:24:42 +0100

    Revert "VFS: Fix memory leak caused by concurrently mounting fs with subtype"
    This reverts commit 8033f109be4a1d5b466284e8ab9119c04f2a334b.
    
    Change-Id: I6384b0ed82a4d9b00f9f1de4728a7940a0f884fd

--- fs/namespace.c
+++ fs/namespace.c
@@ -2582,12 +2582,9 @@ static int do_new_mount(struct path *path, const char *fstype, int sb_flags,
 		return -ENODEV;
 
 	mnt = vfs_kern_mount(type, sb_flags, name, data);
-	if (!IS_ERR(mnt) && (type->fs_flags & FS_HAS_SUBTYPE)) {
-		down_write(&mnt->mnt_sb->s_umount);
-		if (!mnt->mnt_sb->s_subtype)
-			mnt = fs_set_subtype(mnt, fstype);
-		up_write(&mnt->mnt_sb->s_umount);
-	}
+	if (!IS_ERR(mnt) && (type->fs_flags & FS_HAS_SUBTYPE) &&
+	    !mnt->mnt_sb->s_subtype)
+		mnt = fs_set_subtype(mnt, fstype);
 
 	put_filesystem(type);
 	if (IS_ERR(mnt))
